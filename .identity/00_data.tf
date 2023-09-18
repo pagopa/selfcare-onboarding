b759a74efa0d78455cacdc3b1250cd37de2f34d6
manuraf <manuel.rafeli@pagopa.it>
2023-09-18 10:42:34 +0200

    add secret jwt public key

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -12,3 +12,8 @@ data "azurerm_key_vault_secret" "key_vault_sonar" {
   name         = "sonar-token"
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
+
+data "azurerm_key_vault_secret" "jwt_public_key" {
+  name         = "jwt-public-key"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
