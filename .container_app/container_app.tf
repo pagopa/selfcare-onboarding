810bf4a16458e61048c6b563146377c7a873ec98
Andrea Grillo <andrea.grillo@pagopa.it>
2023-10-24 10:38:02 +0200

    first commit

---
+++ .container_app/container_app.tf
@@ -0,0 +1,87 @@
+data "azurerm_resource_group" "resource_group_app" {
+  name = "${local.project}-container-app-rg"
+}
+
+data "azurerm_key_vault" "key_vault" {
+  resource_group_name = var.key_vault.resource_group_name
+  name                = var.key_vault.name
+}
+
+data "azurerm_key_vault_secrets" "key_vault_secrets" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "keyvault_secret" {
+  for_each     = toset(data.azurerm_key_vault_secrets.key_vault_secrets.names)
+  name         = each.key
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+data "azurerm_container_app_environment" "container_app_environment" {
+  resource_group_name = data.azurerm_resource_group.resource_group_app.name
+  name                = "${local.project}-cae"
+}
+
+resource "azapi_resource" "container_app_onboarding" {
+  type      = "Microsoft.App/containerApps@2023-05-01"
+  name      = "${local.project}-${local.app_name}"
+  location  = data.azurerm_resource_group.resource_group_app.location
+  parent_id = data.azurerm_resource_group.resource_group_app.id
+
+  tags = var.tags
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  body = jsonencode({
+    properties = {
+      configuration = {
+        activeRevisionsMode = "Single"
+        ingress = {
+          allowInsecure = true
+          external      = true
+          traffic = [
+            {
+              latestRevision = true
+              label          = "latest"
+              weight         = 100
+
+            }
+          ]
+          targetPort = 8080
+        }
+        secrets = local.secrets
+      }
+      environmentId = data.azurerm_container_app_environment.container_app_environment.id
+      template = {
+        containers = [
+          {
+            env   = concat(var.container_app.app_settings, local.secrets_env)
+            image = "ghcr.io/pagopa/selfcare-onboarding-ms:${var.container_app.image_tag}"
+            name  = "${local.project}-${local.app_name}"
+            resources = {
+              cpu    = var.container_app.cpu
+              memory = var.container_app.memory
+            }
+          }
+        ]
+        scale = {
+          maxReplicas = var.container_app.max_replicas
+          minReplicas = var.container_app.min_replicas
+          rules       = var.container_app.scale_rules
+        }
+      }
+    }
+  })
+}
+
+resource "azurerm_key_vault_access_policy" "keyvault_containerapp_access_policy" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azapi_resource.container_app_onboarding.identity[0].principal_id
+
+  secret_permissions = [
+    "Get",
+  ]
+}
---
+++ .container_app/variables.tf
@@ -0,0 +1,73 @@
+variable "prefix" {
+  description = "Domain prefix"
+  type        = string
+  default     = "selc"
+  validation {
+    condition = (
+      length(var.prefix) <= 6
+    )
+    error_message = "Max length is 6 chars."
+  }
+}
+
+variable "env_short" {
+  description = "Environment short name"
+  type        = string
+  validation {
+    condition = (
+      length(var.env_short) <= 1
+    )
+    error_message = "Max length is 1 chars."
+  }
+}
+
+variable "tags" {
+  type = map(any)
+}
+
+variable "container_app" {
+  description = "Container App configuration"
+  type = object({
+    image_tag    = string
+    min_replicas = number
+    max_replicas = number
+
+    scale_rules = list(object({
+      name = string
+      custom = object({
+        metadata = map(string)
+        type     = string
+      })
+    }))
+
+    cpu    = number
+    memory = string
+
+    app_settings = list(object({
+      name  = string
+      value = string
+    }))
+  })
+
+  default = {
+    image_tag    = "latest"
+    min_replicas = 0
+    max_replicas = 1
+
+    scale_rules  = []
+    app_settings = []
+    env          = []
+
+    cpu    = 0.5
+    memory = "1Gi"
+  }
+}
+
+variable "key_vault" {
+  description = "KeyVault data to get secrets values from"
+  type = object({
+    resource_group_name = string
+    name                = string
+    secrets_names       = set(string)
+  })
+}
