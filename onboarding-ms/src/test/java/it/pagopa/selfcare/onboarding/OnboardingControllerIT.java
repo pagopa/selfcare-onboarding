4c78952522aa9cc1d9c71052afc10d16a25edbbb
manuraf <manuel.rafeli@pagopa.it>
2023-10-30 15:58:31 +0100

    remove integration test

--- onboarding-ms/src/test/java/it/pagopa/selfcare/onboarding/OnboardingControllerIT.java
+++
@@ -1,9 +0,0 @@
-package it.pagopa.selfcare.onboarding;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-import it.pagopa.selfcare.onboarding.controller.OnboardingControllerTest;
-
-@QuarkusIntegrationTest
-public class OnboardingControllerIT extends OnboardingControllerTest {
-    // Execute the same tests but in packaged mode.
-}
