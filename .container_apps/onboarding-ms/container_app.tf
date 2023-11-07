6194ebeb3194076a401f9553bcc9155fa185ee4e
Manuel Rafeli <manuel.rafeli@pagopa.it>
2023-11-07 10:29:48 +0100

    add dns private for ca onboarding ms (#35)

--- .container_apps/onboarding-ms/container_app.tf
+++ .container_apps/onboarding-ms/container_app.tf
@@ -22,7 +22,7 @@ data "azurerm_container_app_environment" "container_app_environment" {
   name                = "${local.project}-cae"
 }
 
-resource "azapi_resource" "container_app" {
+resource "azapi_resource" "container_app_onboarding_ms" {
   type      = "Microsoft.App/containerApps@2023-05-01"
   name      = "${local.project}-${local.app_name}-ca"
   location  = data.azurerm_resource_group.resource_group_app.location
@@ -78,9 +78,26 @@ resource "azapi_resource" "container_app" {
 resource "azurerm_key_vault_access_policy" "keyvault_containerapp_access_policy" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azapi_resource.container_app_onboarding.identity[0].principal_id
+  object_id    = azapi_resource.container_app_onboarding_ms.identity[0].principal_id
 
   secret_permissions = [
     "Get",
   ]
+}
+
+data "azurerm_resource_group" "rg_vnet" {
+  name     = format("%s-vnet-rg", local.project)
+}
+
+data "azurerm_private_dns_zone" "private_azurecontainerapps_io" {
+  name                = local.container_app_environment_dns_zone_name
+  resource_group_name = data.azurerm_resource_group.rg_vnet.name
+}
+
+resource "azurerm_private_dns_a_record" "private_dns_record_a_azurecontainerapps_io" {
+  name                = "${azapi_resource.container_app_onboarding_ms.name}.${trimsuffix(data.azurerm_container_app_environment.container_app_environment.default_domain, ".${local.container_app_environment_dns_zone_name}")}"
+  zone_name           = data.azurerm_private_dns_zone.private_azurecontainerapps_io.name
+  resource_group_name = data.azurerm_resource_group.rg_vnet.name
+  ttl                 = 3600
+  records             = [data.azurerm_container_app_environment.container_app_environment.static_ip_address]
 }
