b77bda726d33dcd06fa878a6029c4e82d1594ad4
manuraf <manuel.rafeli@pagopa.it>
2023-11-15 16:05:41 +0100

    load properties by env variables

--- libs/onboarding-sdk-crypto/src/main/java/it/pagopa/selfcare/onboarding/crypto/config/LocalCryptoInitializer.java
+++ libs/onboarding-sdk-crypto/src/main/java/it/pagopa/selfcare/onboarding/crypto/config/LocalCryptoInitializer.java
@@ -1,18 +1,15 @@
 package it.pagopa.selfcare.onboarding.crypto.config;
 
 import it.pagopa.selfcare.onboarding.crypto.utils.CryptoUtils;
-import it.pagopa.selfcare.onboarding.crypto.utils.PropertiesLoader;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Properties;
 
 public class LocalCryptoInitializer {
 
     public static LocalCryptoConfig initializeConfig() {
-        Properties properties = PropertiesLoader.loadProperties("cryptoConfig.properties");
         LocalCryptoConfig config = new LocalCryptoConfig();
-        String cert = properties.getProperty("crypto.key.cert");
-        String pKey = properties.getProperty("crypto.key.private");
+        String cert = System.getProperty("crypto.key.cert");
+        String pKey = System.getProperty("crypto.key.private");
 
         try {
             if(StringUtils.isBlank(cert) || StringUtils.isBlank(pKey)){
