b1fca05af323b178de1eba504d7e1da6afff9093
manuraf <manuel.rafeli@pagopa.it>
2023-11-17 14:36:59 +0100

    read userid uid from jwt

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/controller/OnboardingController.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/controller/OnboardingController.java
@@ -1,5 +1,7 @@
 package it.pagopa.selfcare.onboarding.controller;
 
+import io.quarkus.security.identity.CurrentIdentityAssociation;
+import io.smallrye.jwt.auth.principal.DefaultJWTCallerPrincipal;
 import io.smallrye.mutiny.Uni;
 import it.pagopa.selfcare.onboarding.controller.request.OnboardingDefaultRequest;
 import it.pagopa.selfcare.onboarding.controller.request.OnboardingPaRequest;
@@ -7,50 +9,58 @@ import it.pagopa.selfcare.onboarding.controller.request.OnboardingPspRequest;
 import it.pagopa.selfcare.onboarding.controller.request.OnboardingSaRequest;
 import it.pagopa.selfcare.onboarding.controller.response.OnboardingResponse;
 import it.pagopa.selfcare.onboarding.service.OnboardingService;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
 import jakarta.validation.Valid;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 import lombok.AllArgsConstructor;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
-//@RolesAllowed("USER")
+@RolesAllowed("USER")
 @Path("/onboarding")
 @AllArgsConstructor
 public class OnboardingController {
 
-    final private OnboardingService onboardingService;
+    @Inject
+    CurrentIdentityAssociation currentIdentityAssociation;
 
+    final private OnboardingService onboardingService;
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<OnboardingResponse> onboarding(@Valid OnboardingDefaultRequest onboardingRequest) {
-        return onboardingService.onboarding(onboardingRequest);
+    public Uni<OnboardingResponse> onboarding(@Valid OnboardingDefaultRequest onboardingRequest, @Context SecurityContext ctx) {
+        return readUserIdFromToken(ctx)
+                .onItem().transformToUni(userId -> onboardingService.onboarding(onboardingRequest));
     }
 
     @POST
     @Path("/pa")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<OnboardingResponse> onboardingPa(@Valid OnboardingPaRequest onboardingRequest) {
-        return onboardingService.onboardingPa(onboardingRequest);
+    public Uni<OnboardingResponse> onboardingPa(@Valid OnboardingPaRequest onboardingRequest, @Context SecurityContext ctx) {
+        return readUserIdFromToken(ctx)
+                .onItem().transformToUni(userId -> onboardingService.onboardingPa(onboardingRequest));
     }
 
     @POST
     @Path("/psp")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<OnboardingResponse> onboardingPsp(@Valid OnboardingPspRequest onboardingRequest) {
-        return onboardingService.onboardingPsp(onboardingRequest);
+    public Uni<OnboardingResponse> onboardingPsp(@Valid OnboardingPspRequest onboardingRequest, @Context SecurityContext ctx) {
+        return readUserIdFromToken(ctx)
+                .onItem().transformToUni(userId -> onboardingService.onboardingPsp(onboardingRequest));
     }
 
     @POST
     @Path("/sa")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<OnboardingResponse> onboardingSa(@Valid OnboardingSaRequest onboardingRequest) {
-        return onboardingService.onboardingSa(onboardingRequest);
+    public Uni<OnboardingResponse> onboardingSa(@Valid OnboardingSaRequest onboardingRequest, @Context SecurityContext ctx) {
+        return readUserIdFromToken(ctx)
+                .onItem().transformToUni(userId -> onboardingService.onboardingSa(onboardingRequest));
     }
 
     /**
@@ -65,4 +75,17 @@ public class OnboardingController {
         return onboardingService.onboarding(onboardingMapper.toEntity(onboardingRequest))
                 .map(onboardingMapper::toResponse);
     }*/
+
+
+    private Uni<String> readUserIdFromToken(SecurityContext ctx) {
+
+        return currentIdentityAssociation.getDeferredIdentity()
+                .onItem().transform(identity -> {
+                    if (ctx.getUserPrincipal() == null || !ctx.getUserPrincipal().getName().equals(identity.getPrincipal().getName())) {
+                        throw new InternalServerErrorException("Principal and JsonWebToken names do not match");
+                    }
+                    DefaultJWTCallerPrincipal jwtCallerPrincipal = (DefaultJWTCallerPrincipal) identity.getPrincipal();
+                    return jwtCallerPrincipal.getClaim("uid");
+                });
+    }
 }
