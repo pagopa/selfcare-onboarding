b4888cd354a0f5f22312e1a8b69a2575970b5f14
Manuel Rafeli <manuel.rafeli@pagopa.it>
2023-11-29 14:25:57 +0100

    [SELC-2862] fix: remove SA onboarding (#53)
    * digital address required
    
    * swagger documentation
    
    * remove sa endpoint

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/controller/OnboardingController.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/controller/OnboardingController.java
@@ -7,7 +7,6 @@ import io.smallrye.mutiny.Uni;
 import it.pagopa.selfcare.onboarding.controller.request.OnboardingDefaultRequest;
 import it.pagopa.selfcare.onboarding.controller.request.OnboardingPaRequest;
 import it.pagopa.selfcare.onboarding.controller.request.OnboardingPspRequest;
-import it.pagopa.selfcare.onboarding.controller.request.OnboardingSaRequest;
 import it.pagopa.selfcare.onboarding.controller.response.OnboardingResponse;
 import it.pagopa.selfcare.onboarding.service.OnboardingService;
 import jakarta.inject.Inject;
@@ -19,11 +18,12 @@ import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.SecurityContext;
 import lombok.AllArgsConstructor;
 import org.apache.http.HttpStatus;
+import org.eclipse.microprofile.openapi.annotations.Operation;
 
 import java.io.File;
 
 @Authenticated
-@Path("/onboarding")
+@Path("/v1/onboarding")
 @AllArgsConstructor
 public class OnboardingController {
 
@@ -31,6 +31,10 @@ public class OnboardingController {
     CurrentIdentityAssociation currentIdentityAssociation;
 
     final private OnboardingService onboardingService;
+
+    @Operation(summary = "Perform default onboarding request, it is used for GSP/SA/AS institution type." +
+            "Users data will be saved on personal data vault if it doesn't already exist." +
+            "At the end, function triggers async activities related to onboarding based on institution type.")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -40,6 +44,10 @@ public class OnboardingController {
                 .onItem().transformToUni(ignore -> onboardingService.onboarding(onboardingRequest));
     }
 
+
+    @Operation(summary = "Perform onboarding request for PA institution type, it require billing.recipientCode in additition to default request" +
+            "Users data will be saved on personal data vault if it doesn't already exist." +
+            "At the end, function triggers async activities related to onboarding that consist of create contract and sending mail to institution's digital address.")
     @POST
     @Path("/pa")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -50,6 +58,9 @@ public class OnboardingController {
                 .onItem().transformToUni(ignore -> onboardingService.onboardingPa(onboardingRequest));
     }
 
+    @Operation(summary = "Perform onboarding request for PSP institution type." +
+            "Users data will be saved on personal data vault if it doesn't already exist." +
+            "At the end, function triggers async activities related to onboarding that consist of sending mail to Selfcare admin for approve request.")
     @POST
     @Path("/psp")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -58,16 +69,6 @@ public class OnboardingController {
         return readUserIdFromToken(ctx)
                 .onItem().invoke(onboardingRequest::setUserRequestUid)
                 .onItem().transformToUni(ignore -> onboardingService.onboardingPsp(onboardingRequest));
-    }
-
-    @POST
-    @Path("/sa")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Uni<OnboardingResponse> onboardingSa(@Valid OnboardingSaRequest onboardingRequest, @Context SecurityContext ctx) {
-        return readUserIdFromToken(ctx)
-                .onItem().invoke(onboardingRequest::setUserRequestUid)
-                .onItem().transformToUni(ignore -> onboardingService.onboardingSa(onboardingRequest));
     }
 
     /**
@@ -102,13 +103,17 @@ public class OnboardingController {
     }
 
 
+    @Operation(summary = "Perform complete operation of an onboarding request receiving onboarding id and contract signed by hte institution." +
+            "It checks the contract's signature and upload the contract on an azure storage" +
+            "At the end, function triggers async activities related to complete onboarding " +
+            "that consist of create the institution, activate the onboarding and sending data to notification queue.")
 
     @PUT
     @Path("/{onboardingId}/complete")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Uni<Response> complete(@PathParam(value = "onboardingId") String tokenId, File file) {
-        return onboardingService.complete(tokenId, file)
-                .onItem().transform(ignore -> Response
+    public Uni<Response> complete(@PathParam(value = "onboardingId") String onboardingId, File file) {
+        return onboardingService.complete(onboardingId, file)
+                .map(ignore -> Response
                         .status(HttpStatus.SC_NO_CONTENT)
                         .build());
     }
