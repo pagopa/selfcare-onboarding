f9a9f6789cc37470cc625dea3f60e1ace3e35554
manuraf <manuel.rafeli@pagopa.it>
2023-12-01 15:40:56 +0100

    create institution implementation

---
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/service/CompletionServiceDefault.java
@@ -0,0 +1,97 @@
+package it.pagopa.selfcare.onboarding.service;
+
+import it.pagopa.selfcare.onboarding.common.InstitutionType;
+import it.pagopa.selfcare.onboarding.common.Origin;
+import it.pagopa.selfcare.onboarding.entity.Institution;
+import it.pagopa.selfcare.onboarding.entity.Onboarding;
+import it.pagopa.selfcare.onboarding.exception.GenericOnboardingException;
+import it.pagopa.selfcare.onboarding.mapper.InstitutionMapper;
+import it.pagopa.selfcare.onboarding.repository.OnboardingRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.openapi.quarkus.core_json.api.InstitutionApi;
+import org.openapi.quarkus.core_json.model.InstitutionFromIpaPost;
+import org.openapi.quarkus.core_json.model.InstitutionResponse;
+import org.openapi.quarkus.core_json.model.InstitutionsResponse;
+
+import java.util.Objects;
+
+import static it.pagopa.selfcare.onboarding.common.ProductId.PROD_INTEROP;
+
+@ApplicationScoped
+public class CompletionServiceDefault implements CompletionService {
+
+
+    @RestClient
+    @Inject
+    InstitutionApi institutionApi;
+
+    @Inject
+    InstitutionMapper institutionMapper;
+
+    @Inject
+    OnboardingRepository onboardingRepository;
+
+    @Override
+    public void createInstitutionAndPersistInstitutionId(Onboarding onboarding) {
+
+        Institution institution = onboarding.getInstitution();
+
+        InstitutionsResponse institutionsResponse = institutionApi.getInstitutionsUsingGET(institution.getTaxCode(), institution.getSubunitCode(), null, null);
+        if(Objects.nonNull(institutionsResponse.getInstitutions()) && institutionsResponse.getInstitutions().size() > 1){
+            throw new GenericOnboardingException("List of institutions is ambiguous, it is empty or has more than one element!!");
+        }
+
+        InstitutionResponse institutionResponse =
+                Objects.isNull(institutionsResponse.getInstitutions()) || institutionsResponse.getInstitutions().isEmpty()
+                    ? createInstitution(institution, onboarding.getProductId())
+                    : institutionsResponse.getInstitutions().get(0);
+
+        onboardingRepository
+                .update("institution.id", institutionResponse.getId())
+                .where("_id", onboarding.getOnboardingId());
+    }
+
+    private InstitutionResponse createInstitution(Institution institution, String productId) {
+
+        if (InstitutionType.SA.equals(institution.getInstitutionType())
+                && Origin.ANAC.equals(institution.getOrigin())) {
+
+            return institutionApi.createInstitutionFromAnacUsingPOST(institutionMapper.toInstitutionRequest(institution));
+        }
+
+        if (InstitutionType.AS.equals(institution.getInstitutionType())
+                && Origin.IVASS.equals(institution.getOrigin())) {
+
+            return institutionApi.createInstitutionFromIvassUsingPOST(institutionMapper.toInstitutionRequest(institution));
+        }
+
+        if (InstitutionType.PG.equals(institution.getInstitutionType()) &&
+                (Origin.INFOCAMERE.equals(institution.getOrigin()) || Origin.ADE.equals(institution.getOrigin()))) {
+
+            return institutionApi.createInstitutionFromInfocamereUsingPOST(institutionMapper.toInstitutionRequest(institution));
+        }
+
+        if (InstitutionType.PA.equals(institution.getInstitutionType()) ||
+                InstitutionType.SA.equals(institution.getInstitutionType()) ||
+                (isGspAndProdInterop(institution.getInstitutionType(), productId)
+                        && Origin.IPA.equals(institution.getOrigin()))) {
+
+            InstitutionFromIpaPost fromIpaPost = new InstitutionFromIpaPost();
+            fromIpaPost.setTaxCode(institution.getTaxCode());
+            if(Objects.nonNull(institution.getSubunitType())) {
+                fromIpaPost.setSubunitCode(institution.getSubunitCode());
+                fromIpaPost.setSubunitType(InstitutionFromIpaPost.SubunitTypeEnum.valueOf(institution.getSubunitType().name()));
+            }
+            return  institutionApi.createInstitutionFromIpaUsingPOST(fromIpaPost);
+        }
+
+        return institutionApi.createInstitutionUsingPOST1(institutionMapper.toInstitutionRequest(institution));
+    }
+
+    private boolean isGspAndProdInterop(InstitutionType institutionType, String productId) {
+        return InstitutionType.GSP == institutionType
+                && productId.equals(PROD_INTEROP.getValue());
+    }
+}
---
+++ apps/onboarding-functions/src/test/java/it/pagopa/selfcare/onboarding/service/CompletionServiceDefaultTest.java
@@ -0,0 +1,187 @@
+package it.pagopa.selfcare.onboarding.service;
+
+import io.quarkus.mongodb.panache.common.PanacheUpdate;
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.junit.QuarkusTest;
+import it.pagopa.selfcare.onboarding.common.InstitutionPaSubunitType;
+import it.pagopa.selfcare.onboarding.common.InstitutionType;
+import it.pagopa.selfcare.onboarding.common.Origin;
+import it.pagopa.selfcare.onboarding.entity.Institution;
+import it.pagopa.selfcare.onboarding.entity.Onboarding;
+import it.pagopa.selfcare.onboarding.exception.GenericOnboardingException;
+import it.pagopa.selfcare.onboarding.repository.OnboardingRepository;
+import jakarta.inject.Inject;
+import org.bson.types.ObjectId;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.openapi.quarkus.core_json.api.InstitutionApi;
+import org.openapi.quarkus.core_json.model.InstitutionFromIpaPost;
+import org.openapi.quarkus.core_json.model.InstitutionResponse;
+import org.openapi.quarkus.core_json.model.InstitutionsResponse;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@QuarkusTest
+public class CompletionServiceDefaultTest {
+
+    @Inject
+    CompletionServiceDefault completionServiceDefault;
+
+    @InjectMock
+    OnboardingRepository onboardingRepository;
+
+    @RestClient
+    @InjectMock
+    InstitutionApi institutionApi;
+
+    final String productId = "productId";
+
+    @Test
+    void createInstitutionAndPersistInstitutionId_shouldThrowExceptionIfMoreInstitutions() {
+        Onboarding onboarding = createOnboarding();
+
+        InstitutionsResponse response = new InstitutionsResponse();
+        response.setInstitutions(List.of(new InstitutionResponse(), new InstitutionResponse()));
+        when(institutionApi.getInstitutionsUsingGET(onboarding.getInstitution().getTaxCode(),
+                onboarding.getInstitution().getSubunitCode(), null, null))
+            .thenReturn(response);
+
+        assertThrows(GenericOnboardingException.class, () -> completionServiceDefault.createInstitutionAndPersistInstitutionId(onboarding));
+    }
+
+    @Test
+    void createInstitutionAndPersistInstitutionId_foundInstitution() {
+        Onboarding onboarding = createOnboarding();
+
+        InstitutionsResponse response = new InstitutionsResponse();
+        InstitutionResponse institutionResponse = new InstitutionResponse();
+        institutionResponse.setId("actual-id");
+        response.setInstitutions(List.of(institutionResponse));
+        when(institutionApi.getInstitutionsUsingGET(onboarding.getInstitution().getTaxCode(),
+                onboarding.getInstitution().getSubunitCode(), null, null))
+                .thenReturn(response);
+
+        mockOnboardingUpdateAndExecuteCreateInstitution(onboarding, institutionResponse);
+    }
+
+    void mockOnboardingUpdateAndExecuteCreateInstitution(Onboarding onboarding, InstitutionResponse institutionResponse){
+        PanacheUpdate panacheUpdateMock = mock(PanacheUpdate.class);
+        when(panacheUpdateMock.where("_id", onboarding.getOnboardingId()))
+                .thenReturn(Long.valueOf(1));
+        when(onboardingRepository.update("institution.id", institutionResponse.getId()))
+                .thenReturn(panacheUpdateMock);
+
+        completionServiceDefault.createInstitutionAndPersistInstitutionId(onboarding);
+
+        verify(onboardingRepository, times(1))
+                .update("institution.id", institutionResponse.getId());
+    }
+
+    @Test
+    void createInstitutionAndPersistInstitutionId_notFoundInstitutionAndCreateSaAnac() {
+        Onboarding onboarding = createOnboarding();
+
+        Institution institutionSa = new Institution();
+        institutionSa.setInstitutionType(InstitutionType.SA);
+        institutionSa.setOrigin(Origin.ANAC);
+        onboarding.setInstitution(institutionSa);
+
+        InstitutionsResponse response = new InstitutionsResponse();
+        when(institutionApi.getInstitutionsUsingGET(onboarding.getInstitution().getTaxCode(),
+                onboarding.getInstitution().getSubunitCode(), null, null))
+                .thenReturn(response);
+
+        InstitutionResponse institutionResponse = dummyInstitutionResponse();
+        when(institutionApi.createInstitutionFromAnacUsingPOST(any())).thenReturn(institutionResponse);
+
+        mockOnboardingUpdateAndExecuteCreateInstitution(onboarding, institutionResponse);
+    }
+
+    @Test
+    void createInstitutionAndPersistInstitutionId_notFoundInstitutionAndCreateAsIvass() {
+        Onboarding onboarding = createOnboarding();
+
+        Institution institution = new Institution();
+        institution.setInstitutionType(InstitutionType.AS);
+        institution.setOrigin(Origin.IVASS);
+        onboarding.setInstitution(institution);
+
+        InstitutionsResponse response = new InstitutionsResponse();
+        when(institutionApi.getInstitutionsUsingGET(onboarding.getInstitution().getTaxCode(),
+                onboarding.getInstitution().getSubunitCode(), null, null))
+                .thenReturn(response);
+
+        InstitutionResponse institutionResponse = dummyInstitutionResponse();
+        when(institutionApi.createInstitutionFromIvassUsingPOST(any())).thenReturn(institutionResponse);
+
+        mockOnboardingUpdateAndExecuteCreateInstitution(onboarding, institutionResponse);
+    }
+    @Test
+    void createInstitutionAndPersistInstitutionId_notFoundInstitutionAndCreatePgAde() {
+        Onboarding onboarding = createOnboarding();
+
+        Institution institution = new Institution();
+        institution.setInstitutionType(InstitutionType.PG);
+        institution.setOrigin(Origin.ADE);
+        onboarding.setInstitution(institution);
+
+        InstitutionsResponse response = new InstitutionsResponse();
+        when(institutionApi.getInstitutionsUsingGET(onboarding.getInstitution().getTaxCode(),
+                onboarding.getInstitution().getSubunitCode(), null, null))
+                .thenReturn(response);
+
+        InstitutionResponse institutionResponse = dummyInstitutionResponse();
+        when(institutionApi.createInstitutionFromInfocamereUsingPOST(any())).thenReturn(institutionResponse);
+
+        mockOnboardingUpdateAndExecuteCreateInstitution(onboarding, institutionResponse);
+    }
+    @Test
+    void createInstitutionAndPersistInstitutionId_notFoundInstitutionAndCreatePa() {
+        Onboarding onboarding = createOnboarding();
+
+        Institution institution = new Institution();
+        institution.setInstitutionType(InstitutionType.PA);
+        institution.setSubunitType(InstitutionPaSubunitType.AOO);
+        institution.setSubunitCode("code");
+        onboarding.setInstitution(institution);
+
+        InstitutionsResponse response = new InstitutionsResponse();
+        when(institutionApi.getInstitutionsUsingGET(onboarding.getInstitution().getTaxCode(),
+                onboarding.getInstitution().getSubunitCode(), null, null))
+                .thenReturn(response);
+
+        InstitutionResponse institutionResponse = dummyInstitutionResponse();
+        when(institutionApi.createInstitutionFromIpaUsingPOST(any())).thenReturn(institutionResponse);
+
+        mockOnboardingUpdateAndExecuteCreateInstitution(onboarding, institutionResponse);
+
+        ArgumentCaptor<InstitutionFromIpaPost> captor = ArgumentCaptor.forClass(InstitutionFromIpaPost.class);
+        verify(institutionApi, times(1))
+                .createInstitutionFromIpaUsingPOST(captor.capture());
+        assertEquals(institution.getTaxCode(), captor.getValue().getTaxCode());
+        assertEquals(institution.getSubunitCode(), captor.getValue().getSubunitCode());
+    }
+
+    private InstitutionResponse dummyInstitutionResponse() {
+        InstitutionResponse response = new InstitutionResponse();
+        response.setId("response-id");
+        return  response;
+    }
+
+
+    private Onboarding createOnboarding() {
+        Onboarding onboarding = new Onboarding();
+        onboarding.setId(ObjectId.get());
+        onboarding.setOnboardingId(onboarding.getId().toHexString());
+        onboarding.setProductId(productId);
+        onboarding.setUsers(List.of());
+        onboarding.setInstitution(new Institution());
+        onboarding.setUserRequestUid("example-uid");
+        return onboarding;
+    }
+}
