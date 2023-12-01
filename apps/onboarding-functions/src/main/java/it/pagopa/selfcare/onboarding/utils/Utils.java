2944391a270011cd60233171e5c301c920b13f26
Manuel Rafeli <manuel.rafeli@pagopa.it>
2023-12-01 09:47:09 +0100

    [SELC-3452] feat: completion async orchestration (#58)
    * start orchestration
    
    * orchestration

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/utils/Utils.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/utils/Utils.java
@@ -1,5 +1,10 @@
 package it.pagopa.selfcare.onboarding.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.pagopa.selfcare.onboarding.entity.Onboarding;
+import it.pagopa.selfcare.onboarding.exception.FunctionOrchestratedException;
+
 import java.util.function.Function;
 
 public class Utils {
@@ -8,4 +13,25 @@ public class Utils {
 
     public static final Function<String, String> CONTRACT_FILENAME_FUNC =
             productName -> String.format(PDF_FORMAT_FILENAME, productName.replaceAll("\\s+","_"));
+
+
+
+    public static Onboarding readOnboardingValue(ObjectMapper objectMapper, String onboardingString) {
+        try {
+            return objectMapper.readValue(onboardingString, Onboarding.class);
+        } catch (JsonProcessingException e) {
+            throw new FunctionOrchestratedException(e);
+        }
+    }
+
+    public static String getOnboardingString(ObjectMapper objectMapper, Onboarding onboarding) {
+
+        String onboardingString;
+        try {
+            onboardingString = objectMapper.writeValueAsString(onboarding);
+        } catch (JsonProcessingException e) {
+            throw new FunctionOrchestratedException(e);
+        }
+        return onboardingString;
+    }
 }
