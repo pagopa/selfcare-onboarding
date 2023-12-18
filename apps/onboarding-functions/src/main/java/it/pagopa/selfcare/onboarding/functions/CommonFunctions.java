3ad181425da6244cedb54518d443a520f8a59970
manuraf <manuel.rafeli@pagopa.it>
2023-12-18 17:52:34 +0100

    purge functions

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/CommonFunctions.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/CommonFunctions.java
@@ -2,10 +2,22 @@ package it.pagopa.selfcare.onboarding.functions;
 
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.TimerTrigger;
+import com.microsoft.durabletask.OrchestrationRuntimeStatus;
+import com.microsoft.durabletask.PurgeInstanceCriteria;
+import com.microsoft.durabletask.PurgeResult;
 import com.microsoft.durabletask.azurefunctions.DurableActivityTrigger;
+import com.microsoft.durabletask.azurefunctions.DurableClientContext;
+import com.microsoft.durabletask.azurefunctions.DurableClientInput;
 import it.pagopa.selfcare.onboarding.common.OnboardingStatus;
+import it.pagopa.selfcare.onboarding.config.PurgeConfig;
 import it.pagopa.selfcare.onboarding.functions.utils.SaveOnboardingStatusInput;
 import it.pagopa.selfcare.onboarding.service.OnboardingService;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 public class CommonFunctions {
 
@@ -14,8 +26,11 @@ public class CommonFunctions {
 
     private final OnboardingService service;
 
-    public CommonFunctions(OnboardingService service) {
+    private final PurgeConfig purgeConfig;
+
+    public CommonFunctions(OnboardingService service, PurgeConfig purgeConfig) {
         this.service = service;
+        this.purgeConfig = purgeConfig;
     }
 
     @FunctionName(SAVE_ONBOARDING_STATUS_ACTIVITY)
@@ -23,5 +38,30 @@ public class CommonFunctions {
         SaveOnboardingStatusInput saveOnboardingStatusInput = SaveOnboardingStatusInput.readJsonString(saveOnboardingStatusInputString);
         context.getLogger().info(String.format(FORMAT_LOGGER_ONBOARDING_STRING, SAVE_ONBOARDING_STATUS_ACTIVITY, saveOnboardingStatusInput.getOnboardingId()));
         service.savePendingState(saveOnboardingStatusInput.getOnboardingId(), OnboardingStatus.valueOf(saveOnboardingStatusInput.getStatus()));
+    }
+
+    @FunctionName("PurgeInstancesCompleted")
+    public void purgeInstances(
+            @TimerTrigger(name = "purgeTimer", schedule = "0 0 11 * * *") String timerInfo,
+            @DurableClientInput(name = "durableContext") DurableClientContext durableContext,
+            ExecutionContext context) throws TimeoutException {
+        PurgeInstanceCriteria criteria = new PurgeInstanceCriteria()
+                .setCreatedTimeFrom(Instant.now().minus(Duration.ofDays(purgeConfig.completedFrom())))
+                .setCreatedTimeTo(Instant.now().minus(Duration.ofDays(purgeConfig.completedTo())))
+                .setRuntimeStatusList(List.of(OrchestrationRuntimeStatus.COMPLETED));
+        PurgeResult result = durableContext.getClient().purgeInstances(criteria);
+        context.getLogger().info(String.format("Purged %d instance(s)", result.getDeletedInstanceCount()));
+    }
+
+    @FunctionName("PurgeInstancesAll")
+    public void purgeInstancesAll(
+            @TimerTrigger(name = "purgeTimer", schedule = "0 0 12 * * *") String timerInfo,
+            @DurableClientInput(name = "durableContext") DurableClientContext durableContext,
+            ExecutionContext context) throws TimeoutException {
+        PurgeInstanceCriteria criteria = new PurgeInstanceCriteria()
+                .setCreatedTimeFrom(Instant.now().minus(Duration.ofDays(purgeConfig.allFrom())))
+                .setCreatedTimeTo(Instant.now().minus(Duration.ofDays(purgeConfig.allTo())));
+        PurgeResult result = durableContext.getClient().purgeInstances(criteria);
+        context.getLogger().info(String.format("Purged %d instance(s)", result.getDeletedInstanceCount()));
     }
 }
