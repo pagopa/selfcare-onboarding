42a022556afc45d68a1e3348ae08eaf7490e4556
Andrea Grillo <andrea.grillo@pagopa.it>
2024-01-10 12:51:02 +0100

    add function

---
+++ infra/functions/onboarding-functions/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  project  = "${var.prefix}-${var.env_short}"
+  app_name = "onboarding-functions"
+
+  monitor_rg_name          = "${local.project}-monitor-rg"
+  monitor_appinsights_name = "${local.project}-appinsights"
+}
