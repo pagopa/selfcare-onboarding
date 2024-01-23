dc197fbf8b4e4a7f6adb438b1b91307c56cc5226
manuraf <manuel.rafeli@pagopa.it>
2024-01-23 18:06:20 +0100

    workflow confirmation

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/workflow/WorkflowExecutorConfirmation.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/workflow/WorkflowExecutorConfirmation.java
@@ -3,13 +3,7 @@ package it.pagopa.selfcare.onboarding.workflow;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.durabletask.TaskOptions;
 import com.microsoft.durabletask.TaskOrchestrationContext;
-import it.pagopa.selfcare.onboarding.common.OnboardingStatus;
 import it.pagopa.selfcare.onboarding.entity.Onboarding;
-
-import static it.pagopa.selfcare.onboarding.functions.CommonFunctions.SAVE_ONBOARDING_STATUS_ACTIVITY;
-import static it.pagopa.selfcare.onboarding.functions.utils.ActivityName.SEND_MAIL_CONFIRMATION_ACTIVITY;
-import static it.pagopa.selfcare.onboarding.functions.utils.SaveOnboardingStatusInput.buildAsJsonString;
-import static it.pagopa.selfcare.onboarding.utils.Utils.getOnboardingString;
 
 public class WorkflowExecutorConfirmation implements WorkflowExecutor {
 
@@ -22,9 +16,6 @@ public class WorkflowExecutorConfirmation implements WorkflowExecutor {
     }
     @Override
     public void executeRequestState(TaskOrchestrationContext ctx, Onboarding onboarding) {
-        String onboardingString = getOnboardingString(objectMapper, onboarding);
-        ctx.callActivity(SEND_MAIL_CONFIRMATION_ACTIVITY, onboardingString, optionsRetry, String.class).await();
-        ctx.callActivity(SAVE_ONBOARDING_STATUS_ACTIVITY, buildAsJsonString(onboarding.getOnboardingId(), OnboardingStatus.PENDING.name()), optionsRetry, String.class).await();
     }
 
     @Override
