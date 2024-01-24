4c678213473c53e6af5a54b9cf2f20aee8631d97
manuraf <manuel.rafeli@pagopa.it>
2024-01-24 12:37:27 +0100

    remove savePendingState functions

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/CommonFunctions.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/CommonFunctions.java
@@ -6,13 +6,9 @@ import com.microsoft.azure.functions.annotation.TimerTrigger;
 import com.microsoft.durabletask.OrchestrationRuntimeStatus;
 import com.microsoft.durabletask.PurgeInstanceCriteria;
 import com.microsoft.durabletask.PurgeResult;
-import com.microsoft.durabletask.azurefunctions.DurableActivityTrigger;
 import com.microsoft.durabletask.azurefunctions.DurableClientContext;
 import com.microsoft.durabletask.azurefunctions.DurableClientInput;
-import it.pagopa.selfcare.onboarding.common.OnboardingStatus;
 import it.pagopa.selfcare.onboarding.config.PurgeConfig;
-import it.pagopa.selfcare.onboarding.functions.utils.SaveOnboardingStatusInput;
-import it.pagopa.selfcare.onboarding.service.OnboardingService;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -22,22 +18,11 @@ import java.util.concurrent.TimeoutException;
 public class CommonFunctions {
 
     public static final String FORMAT_LOGGER_ONBOARDING_STRING = "%s: %s";
-    public static final String SAVE_ONBOARDING_STATUS_ACTIVITY = "SaveOnboardingStatus";
-
-    private final OnboardingService service;
 
     private final PurgeConfig purgeConfig;
 
-    public CommonFunctions(OnboardingService service, PurgeConfig purgeConfig) {
-        this.service = service;
+    public CommonFunctions(PurgeConfig purgeConfig) {
         this.purgeConfig = purgeConfig;
-    }
-
-    @FunctionName(SAVE_ONBOARDING_STATUS_ACTIVITY)
-    public void savePendingState(@DurableActivityTrigger(name = "onboardingString") String saveOnboardingStatusInputString, final ExecutionContext context) {
-        SaveOnboardingStatusInput saveOnboardingStatusInput = SaveOnboardingStatusInput.readJsonString(saveOnboardingStatusInputString);
-        context.getLogger().info(String.format(FORMAT_LOGGER_ONBOARDING_STRING, SAVE_ONBOARDING_STATUS_ACTIVITY, saveOnboardingStatusInput.getOnboardingId()));
-        service.savePendingState(saveOnboardingStatusInput.getOnboardingId(), OnboardingStatus.valueOf(saveOnboardingStatusInput.getStatus()));
     }
 
     @FunctionName("PurgeInstancesCompleted")
