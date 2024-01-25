fbb7b2ca8860063ad567142fc7cb21c0c3f42ad4
Manuel Rafeli <manuel.rafeli@pagopa.it>
2024-01-25 16:53:20 +0100

    [SELC-3882] fix: TOBEVALIDATED step when APPROVE_PT (#121)
    * fix call default method
    
    * unit test

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/workflow/WorkflowExecutorForApprovePt.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/workflow/WorkflowExecutorForApprovePt.java
@@ -31,7 +31,7 @@ public class WorkflowExecutorForApprovePt implements WorkflowExecutor {
 
     @Override
     public Optional<OnboardingStatus> executeToBeValidatedState(TaskOrchestrationContext ctx, Onboarding onboarding) {
-        return executePendingState(ctx, onboarding);
+        return WorkflowExecutor.super.executePendingState(ctx, onboarding);
     }
 
     @Override
