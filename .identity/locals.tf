521248864ef4560b18ded40d7d69031879d986e8
manuraf <manuel.rafeli@pagopa.it>
2024-02-07 11:31:07 +0100

    refactor repo vars

--- .identity/locals.tf
+++ .identity/locals.tf
@@ -9,23 +9,27 @@ locals {
     cd_branch_policy_enabled = var.github_repository_environment_cd.protected_branches == true || var.github_repository_environment_cd.custom_branch_policies == true
   }
 
+  repo_variables = {
+    "AZURE_TENANT_ID" = data.azurerm_client_config.current.tenant_id,
+  }
+
   repo_secrets = {
+    "SONAR_TOKEN" = data.azurerm_key_vault_secret.key_vault_sonar.value,
+  }
+
+  env_ci_variables = {
     "AZURE_SUBSCRIPTION_ID" = data.azurerm_client_config.current.subscription_id
-    "AZURE_TENANT_ID"       = data.azurerm_client_config.current.tenant_id,
-    "SONAR_TOKEN"           = data.azurerm_key_vault_secret.key_vault_sonar.value,
   }
 
   env_cd_variables = {
-    "AZURE_ONBOARDING_FN_APP_NAME"       = "${local.project}-onboarding-fn",
-    "AZURE_ONBOARDING_FN_RESOURCE_GROUP" = "${local.project}-onboarding-fn-rg",
-    "AZURE_ONBOARDING_FN_SERVICE_PLAN"   = "${local.project}-onboarding-fn-plan"
+    "AZURE_SUBSCRIPTION_ID" = data.azurerm_client_config.current.subscription_id
   }
 
   env_ci_secrets = {
-    "AZURE_CLIENT_ID_CI" = module.identity_ci.identity_client_id
+    "AZURE_CLIENT_ID" = module.identity_ci.identity_client_id
   }
 
   env_cd_secrets = {
-    "AZURE_CLIENT_ID_CD" = module.identity_cd.identity_client_id
+    "AZURE_CLIENT_ID" = module.identity_cd.identity_client_id
   }
 }
