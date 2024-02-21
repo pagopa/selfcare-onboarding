02ed38d44dc6e505e84fc7a9d0f1b69f750c9cba
manuraf <manuel.rafeli@pagopa.it>
2024-02-21 15:12:40 +0100

    return problem entity on invalidrequest

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/exception/handler/ExceptionHandler.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/exception/handler/ExceptionHandler.java
@@ -4,11 +4,15 @@ import it.pagopa.selfcare.onboarding.exception.InvalidRequestException;
 import it.pagopa.selfcare.onboarding.exception.OnboardingNotAllowedException;
 import it.pagopa.selfcare.onboarding.exception.ResourceConflictException;
 import it.pagopa.selfcare.onboarding.exception.ResourceNotFoundException;
+import it.pagopa.selfcare.onboarding.exception.model.Problem;
+import it.pagopa.selfcare.onboarding.exception.model.ProblemError;
 import jakarta.ws.rs.core.Response;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 
 public class ExceptionHandler {
@@ -17,9 +21,13 @@ public class ExceptionHandler {
     public static final String SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER = "Something has gone wrong in the server";
 
     @ServerExceptionMapper
-    public RestResponse<String> toResponse(InvalidRequestException exception) {
+    public Response toResponse(InvalidRequestException exception) {
         LOGGER.error("{}: {}", SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
-        return RestResponse.status(Response.Status.BAD_REQUEST, exception.getMessage());
+        Problem problem = problem(exception.getMessage(), Response.Status.BAD_REQUEST.getStatusCode(), exception.getCode());
+        return Response
+                .status(Response.Status.BAD_REQUEST)
+                .entity(problem)
+                .build();
     }
     @ServerExceptionMapper
     public RestResponse<String> toResponse(OnboardingNotAllowedException exception) {
@@ -41,5 +49,15 @@ public class ExceptionHandler {
     public RestResponse<String> toResponse(ResourceConflictException exception) {
         LOGGER.error("{}: {}", SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
         return RestResponse.status(Response.Status.CONFLICT, exception.getMessage());
+    }
+
+    private Problem problem(String errorMessage, Integer status, String code) {
+        Problem problem = new Problem();
+        problem.setStatus(status);
+        problem.setErrors(List.of(ProblemError.builder()
+                .code(code)
+                .detail(errorMessage)
+                .build()));
+        return problem;
     }
 }
