b91e3b780a021f43352116632216e14db02ac2f1
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-05-03 10:42:16 +0200

    Removed condition on field originId for IVASS contract

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/utils/PdfMapper.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/utils/PdfMapper.java
@@ -61,7 +61,7 @@ public class PdfMapper {
         map.put("zipCode", Optional.ofNullable(institution.getZipCode()).orElse(""));
         map.put("managerName", getStringValue(manager.getName()));
         map.put("managerSurname", getStringValue(manager.getFamilyName()));
-        map.put("originId", Optional.ofNullable(institution.getTaxCode()).map(taxCode -> UNDERSCORE).orElse(Optional.ofNullable(institution.getOriginId()).orElse(UNDERSCORE)));
+        map.put("originId", Optional.ofNullable(institution.getOriginId()).orElse(UNDERSCORE));
         map.put("institutionMail", institution.getDigitalAddress());
         map.put("managerTaxCode", manager.getFiscalCode());
         map.put("managerEmail", mailManager);
