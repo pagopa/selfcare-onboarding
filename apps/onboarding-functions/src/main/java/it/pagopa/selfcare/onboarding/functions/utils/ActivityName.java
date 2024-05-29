2ea042ffeb084551ae341c8e0bff61b56b68b73f
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-05-29 16:20:44 +0200

    Added private constructor to avoid construction of this class

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/utils/ActivityName.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/utils/ActivityName.java
@@ -10,9 +10,6 @@ public class ActivityName {
     public static final String SEND_MAIL_REGISTRATION_REQUEST_ACTIVITY = "SendMailRegistrationRequest";
     public static final String SEND_MAIL_REGISTRATION_APPROVE_ACTIVITY = "SendMailRegistrationApprove";
     public static final String SEND_MAIL_ONBOARDING_APPROVE_ACTIVITY = "SendMailOnboardingApprove";
-
-
-    public static final String ONBOARDING_COMPLETION_ACTIVITY = "OnboardingCompletion";
     public static final String CREATE_INSTITUTION_ACTIVITY = "CreateInstitution";
     public static final String CREATE_ONBOARDING_ACTIVITY = "CreateOnboarding";
     public static final String SEND_MAIL_COMPLETION_ACTIVITY = "SendMailCompletion";
@@ -21,4 +18,7 @@ public class ActivityName {
     public static final String CREATE_USERS_ACTIVITY = "CreateUsers";
 
     public static final String SEND_ONBOARDING_NOTIFICATION = "SendOnboardingNotification";
+
+    private ActivityName() {
+    }
 }
