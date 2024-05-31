c70f1cfb3a5f2fb12d50717b0b0a60b731e8844d
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2024-05-31 10:10:35 +0200

    feat: added sent notification logic

--- apps/onboarding-cdc/src/main/java/it/pagopa/selfcare/onboarding/event/OnboardingCdcService.java
+++ apps/onboarding-cdc/src/main/java/it/pagopa/selfcare/onboarding/event/OnboardingCdcService.java
@@ -17,12 +17,19 @@ import io.quarkus.runtime.configuration.ConfigUtils;
 import io.smallrye.mutiny.Multi;
 import it.pagopa.selfcare.onboarding.event.constant.CdcStartAtConstant;
 import it.pagopa.selfcare.onboarding.event.entity.Onboarding;
+import it.pagopa.selfcare.onboarding.event.mapper.OnboardingMapper;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.BsonDocument;
 import org.bson.conversions.Bson;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.openapi.quarkus.onboarding_functions_json.api.NotificationsApi;
+
+import java.time.Duration;
 import java.util.*;
+
 import static com.mongodb.client.model.Projections.fields;
 import static com.mongodb.client.model.Projections.include;
 import static java.util.Arrays.asList;
@@ -32,6 +39,11 @@ import static java.util.Arrays.asList;
 @ApplicationScoped
 public class OnboardingCdcService {
 
+    @Inject
+    @RestClient
+    NotificationsApi notificationsApi;
+    @Inject
+    OnboardingMapper onboardingMapper;
     private static final String COLLECTION_NAME = "onboardings";
     private static final String OPERATION_NAME = "ONBOARDING-CDC-OnboardingsUpdate";
     private static final String EVENT_NAME = "ONBOARDING-CDC";
@@ -70,7 +82,7 @@ public class OnboardingCdcService {
         //Retrieve last resumeToken for watching collection at specific operation
         String resumeToken = null;
 
-        if(!ConfigUtils.getProfiles().contains("test")) {
+        if (!ConfigUtils.getProfiles().contains("test")) {
             try {
                 TableEntity cdcStartAtEntity = tableClient.getEntity(CdcStartAtConstant.CDC_START_AT_PARTITION_KEY, CdcStartAtConstant.CDC_START_AT_ROW_KEY);
                 if (Objects.nonNull(cdcStartAtEntity))
@@ -84,19 +96,20 @@ public class OnboardingCdcService {
         ReactiveMongoCollection<Onboarding> dataCollection = getCollection();
         ChangeStreamOptions options = new ChangeStreamOptions()
                 .fullDocument(FullDocument.UPDATE_LOOKUP);
-        if(Objects.nonNull(resumeToken))
+        if (Objects.nonNull(resumeToken))
             options = options.resumeAfter(BsonDocument.parse(resumeToken));
 
         Bson match = Aggregates.match(Filters.in("operationType", asList("update", "replace", "insert")));
+        Bson matchStatus = Aggregates.match(Filters.in("fullDocument.status", Arrays.asList("COMPLETED", "DELETED")));
         Bson project = Aggregates.project(fields(include("_id", "ns", "documentKey", "fullDocument")));
-        List<Bson> pipeline = Arrays.asList(match, project);
+        List<Bson> pipeline = Arrays.asList(match, matchStatus, project);
 
         Multi<ChangeStreamDocument<Onboarding>> publisher = dataCollection.watch(pipeline, Onboarding.class, options);
         publisher.subscribe().with(
                 this::consumerOnboardingEvent,
                 failure -> {
                     log.error("Error during subscribe collection, exception: {} , message: {}", failure.toString(), failure.getMessage());
-                    constructMapAndTrackEvent(failure.getClass().toString(), "FALSE", ONBOARDING_FAILURE_MECTRICS);
+                    constructMapAndTrackEvent(failure.getClass().toString(), ONBOARDING_FAILURE_MECTRICS);
                     Quarkus.asyncExit();
                 });
 
@@ -111,27 +124,26 @@ public class OnboardingCdcService {
 
     protected void consumerOnboardingEvent(ChangeStreamDocument<Onboarding> document) {
 
+
         assert document.getFullDocument() != null;
         assert document.getDocumentKey() != null;
 
         log.info("Starting consumerOnboardingEvent ... ");
-/*
-        userInstitutionRepository.updateUser(document.getFullDocument())
+        log.info("Sending Onboarding notification having id {}", document.getFullDocument().getId());
+
+        notificationsApi.apiNotificationsPost(onboardingMapper.toEntity(document.getFullDocument()))
                 .onFailure().retry().withBackOff(Duration.ofSeconds(retryMinBackOff), Duration.ofHours(retryMaxBackOff)).atMost(maxRetry)
                 .subscribe().with(
                         result -> {
-                            log.info("UserInfo collection successfully updated from UserInstitution document having id: {}", document.getDocumentKey().toJson());
+                            log.info("Onboarding notification having id: {} successfully sent", document.getDocumentKey().toJson());
                             updateLastResumeToken(document.getResumeToken());
                             constructMapAndTrackEvent(document.getDocumentKey().toJson(), "TRUE", ONBOARDING_SUCCESS_MECTRICS);
                         },
                         failure -> {
-                            log.error("Error during UserInfo collection updating, from UserInstitution document having id: {} , message: {}", document.getDocumentKey().toJson(), failure.getMessage());
+                            log.error("Error during send Onboarding notifiction having id: {} , message: {}", document.getDocumentKey().toJson(), failure.getMessage());
                             constructMapAndTrackEvent(document.getDocumentKey().toJson(), "FALSE", ONBOARDING_FAILURE_MECTRICS);
                         });
- */
-        log.info("Document : {}", document.getFullDocument());
-        //Codice per invio dei messaggi su coda
-
+        log.info("End consumerOnboardingEvent ... ");
     }
 
     private void updateLastResumeToken(BsonDocument resumeToken) {
@@ -155,3 +167,4 @@ public class OnboardingCdcService {
         telemetryClient.trackEvent(EVENT_NAME, propertiesMap, metricsMap);
     }
 }
+
