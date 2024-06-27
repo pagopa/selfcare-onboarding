7ffe220dcc8ee7da8c10e7cfbb5863eedbbb0f39
empassaro <emanuele.passaro@emeal.nttdata.com>
2024-06-27 18:03:14 +0200

    SELC-5191 feat: added function to check if onboarding should send notification

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/utils/Utils.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/utils/Utils.java
@@ -2,21 +2,28 @@ package it.pagopa.selfcare.onboarding.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import it.pagopa.selfcare.onboarding.common.WorkflowType;
 import it.pagopa.selfcare.onboarding.dto.OnboardingAggregateOrchestratorInput;
 import it.pagopa.selfcare.onboarding.entity.Onboarding;
 import it.pagopa.selfcare.onboarding.entity.OnboardingWorkflow;
 import it.pagopa.selfcare.onboarding.exception.FunctionOrchestratedException;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.List;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 
 public class Utils {
 
     public static final BiFunction<String, String, String> CONTRACT_FILENAME_FUNC =
             (filename, productName) -> String.format(filename, StringUtils.stripAccents(productName.replaceAll("\\s+","_")));
 
-
+    private static final List<WorkflowType> ALLOWED_WORKFLOWS_FOR_INSTITUTION_NOTIFICATIONS = List.of(
+            WorkflowType.CONFIRMATION,
+            WorkflowType.FOR_APPROVE,
+            WorkflowType.IMPORT,
+            WorkflowType.CONTRACT_REGISTRATION,
+            WorkflowType.FOR_APPROVE_PT
+    );
 
     public static Onboarding readOnboardingValue(ObjectMapper objectMapper, String onboardingString) {
         try {
@@ -62,5 +69,9 @@ public class Utils {
             throw new FunctionOrchestratedException(e);
         }
         return onboardingWorkflowString;
+    }
+
+    public static boolean isNotInstitutionOnboarding(Onboarding onboarding) {
+        return !ALLOWED_WORKFLOWS_FOR_INSTITUTION_NOTIFICATIONS.contains(onboarding.getWorkflowType());
     }
 }
