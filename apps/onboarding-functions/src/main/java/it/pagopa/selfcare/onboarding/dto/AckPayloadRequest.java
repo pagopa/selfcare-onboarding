4a13f95b70e593857003d79627de2a6434ac7a71
empassaro <113031808+empassaro@users.noreply.github.com>
2024-06-27 18:08:48 +0200

    [SELC-4921] feat: added function for messageAcknowledgment (#348)

---
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/dto/AckPayloadRequest.java
@@ -0,0 +1,14 @@
+package it.pagopa.selfcare.onboarding.dto;
+
+public class AckPayloadRequest {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/ExternalFunctions.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/ExternalFunctions.java
@@ -1,18 +1,27 @@
 package it.pagopa.selfcare.onboarding.functions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import it.pagopa.selfcare.onboarding.dto.AckPayloadRequest;
 import it.pagopa.selfcare.onboarding.service.CheckOrganizationService;
+import it.pagopa.selfcare.onboarding.utils.AckStatus;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Optional;
 
+import static it.pagopa.selfcare.onboarding.utils.Utils.readAckPayloadValue;
+
 public class ExternalFunctions {
     private final CheckOrganizationService checkOrganizationService;
+    private final ObjectMapper objectMapper;
 
-    public ExternalFunctions(CheckOrganizationService checkOrganizationService) {
+    public ExternalFunctions(CheckOrganizationService checkOrganizationService, ObjectMapper objectMapper) {
         this.checkOrganizationService = checkOrganizationService;
+        this.objectMapper = objectMapper;
     }
 
     @FunctionName("TestToken")
@@ -51,5 +60,42 @@ public class ExternalFunctions {
         } else {
             return request.createResponseBuilder(HttpStatus.NOT_FOUND).build();
         }
+    }
+
+    @FunctionName("messageAcknowledgment")
+    public HttpResponseMessage messageAcknowledgment(
+            @HttpTrigger(name = "req", methods = {HttpMethod.POST}, route = "acknowledgment/{productId}/message/{messageId}/status/{status}", authLevel = AuthorizationLevel.FUNCTION)
+            HttpRequestMessage<Optional<String>> request, @BindingName("productId") String productId, @BindingName("messageId") String messageId, @BindingName("status") String status, final ExecutionContext context) {
+        context.getLogger().info("messageAcknowledgment trigger processed a request");
+
+        final AckPayloadRequest ackPayloadRequest;
+        try {
+            ackPayloadRequest = retrieveAckPayloadRequest(request);
+        } catch (IllegalArgumentException e) {
+            context.getLogger().warning(e::getMessage);
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage())
+                    .build();
+        }
+
+        context.getLogger().info(() -> String.format("productId = %s, messageId = %s, status = %s, payload = %s", productId, messageId, status, ackPayloadRequest.getMessage()));
+        if (AckStatus.ACK.equals(AckStatus.valueOf(status))) {
+            context.getLogger().info(() -> String.format("[SUCCESSFUL Acknowledgment] - Consumer acknowledged message: %s consumption, for product = %s", messageId, productId));
+        } else {
+            context.getLogger().warning(() -> String.format("[ACKNOWLEDGMENT ERROR] - record with %s id gave %s, it wasn't processed correctly by %s, reason = %s", messageId, status, productId, ackPayloadRequest.getMessage()));
+        }
+        context.getLogger().info("messageAcknowledgment end");
+
+        return request.createResponseBuilder(HttpStatus.OK).build();
+    }
+
+    private AckPayloadRequest retrieveAckPayloadRequest(HttpRequestMessage<Optional<String>> request) {
+        String ackPayloadString = request.getBody().orElseThrow(() -> new IllegalArgumentException("Request body cannot be empty."));
+        AckPayloadRequest ackPayloadRequest = readAckPayloadValue(objectMapper, ackPayloadString);
+        if (StringUtils.isBlank(ackPayloadRequest.getMessage())) {
+            throw new IllegalArgumentException("Field message is required in request body and cannot be blank.");
+        }
+
+        return ackPayloadRequest;
     }
 }
