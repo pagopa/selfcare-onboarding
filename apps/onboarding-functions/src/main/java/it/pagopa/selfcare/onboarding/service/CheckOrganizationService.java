7bf01b523dbcd006a4cbd4d98bddbc0120d939e9
empassaro <113031808+empassaro@users.noreply.github.com>
2024-06-27 15:56:27 +0200

    Feature/test fd token (#355)

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/service/CheckOrganizationService.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/service/CheckOrganizationService.java
@@ -4,4 +4,5 @@ import com.microsoft.azure.functions.ExecutionContext;
 
 public interface CheckOrganizationService {
     boolean checkOrganization(ExecutionContext context, String fiscalCode, String vatNumber);
+    String testToken(ExecutionContext context);
 }
