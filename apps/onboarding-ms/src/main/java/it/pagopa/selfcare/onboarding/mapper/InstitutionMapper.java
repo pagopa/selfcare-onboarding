3fbf1b162ae6068525f25e1951fb341154880a06
pierpaolodidato89 <137791912+pierpaolodidato89@users.noreply.github.com>
2024-07-04 16:48:23 +0200

    [SELC-5215] feat: Added parentDescription into InstitutionMapper

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/mapper/InstitutionMapper.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/mapper/InstitutionMapper.java
@@ -2,11 +2,14 @@ package it.pagopa.selfcare.onboarding.mapper;
 
 import it.pagopa.selfcare.onboarding.entity.Institution;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.openapi.quarkus.core_json.model.InstitutionResponse;
 
 @Mapper(componentModel = "cdi")
 public interface InstitutionMapper {
 
+    @Mapping(source = "rootParent.description", target = "parentDescription", nullValuePropertyMappingStrategy =  NullValuePropertyMappingStrategy.IGNORE)
     Institution toEntity(InstitutionResponse model);
 
 }
