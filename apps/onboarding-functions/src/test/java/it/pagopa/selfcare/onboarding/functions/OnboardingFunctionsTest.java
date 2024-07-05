8950eaa1153743277d58f1ff1b152732ffb76e7a
Giulia Tremolada <124147597+giulia-tremolada@users.noreply.github.com>
2024-07-05 16:20:57 +0200

    [SELC-5095] feat: executePendingState in WorkflowExecutorContractRegistrationAggregator (#384)

--- apps/onboarding-functions/src/test/java/it/pagopa/selfcare/onboarding/functions/OnboardingFunctionsTest.java
+++ apps/onboarding-functions/src/test/java/it/pagopa/selfcare/onboarding/functions/OnboardingFunctionsTest.java
@@ -14,9 +14,11 @@ import io.quarkus.test.junit.QuarkusTest;
 import it.pagopa.selfcare.onboarding.HttpResponseMessageMock;
 import it.pagopa.selfcare.onboarding.common.OnboardingStatus;
 import it.pagopa.selfcare.onboarding.common.WorkflowType;
+import it.pagopa.selfcare.onboarding.entity.AggregateInstitution;
 import it.pagopa.selfcare.onboarding.entity.Institution;
 import it.pagopa.selfcare.onboarding.entity.Onboarding;
 import it.pagopa.selfcare.onboarding.exception.ResourceNotFoundException;
+import it.pagopa.selfcare.onboarding.mapper.OnboardingMapper;
 import it.pagopa.selfcare.onboarding.service.CompletionService;
 import it.pagopa.selfcare.onboarding.service.NotificationEventService;
 import it.pagopa.selfcare.onboarding.service.OnboardingService;
@@ -57,6 +59,9 @@ public class OnboardingFunctionsTest {
 
     @InjectMock
     NotificationEventService notificationEventService;
+
+    @Inject
+    OnboardingMapper onboardingMapper;
 
     final String onboardinString = "{\"onboardingId\":\"onboardingId\"}";
 
@@ -162,6 +167,44 @@ public class OnboardingFunctionsTest {
 
         verify(service, times(1))
                 .updateOnboardingStatus(onboarding.getId(), OnboardingStatus.PENDING);
+
+        function.onboardingsOrchestrator(orchestrationContext, executionContext);
+    }
+
+    @Test
+    void onboardingOrchestratorContractRegistrationAggregator_Pending(){
+        Onboarding onboarding = new Onboarding();
+        onboarding.setId("onboardingId");
+        onboarding.setStatus(OnboardingStatus.PENDING);
+        AggregateInstitution aggregate1 = new AggregateInstitution();
+        aggregate1.setTaxCode("code1");
+        AggregateInstitution aggregate2 = new AggregateInstitution();
+        aggregate1.setTaxCode("code2");
+        AggregateInstitution aggregate3 = new AggregateInstitution();
+        aggregate1.setTaxCode("code3");
+        onboarding.setAggregates(List.of(aggregate1, aggregate2, aggregate3));
+        Institution institution = new Institution();
+        institution.setId("id");
+        onboarding.setInstitution(institution);
+        onboarding.setWorkflowType(WorkflowType.CONTRACT_REGISTRATION_AGGREGATOR);
+
+        TaskOrchestrationContext orchestrationContext = mockTaskOrchestrationContext(onboarding);
+        function.onboardingsOrchestrator(orchestrationContext, executionContext);
+
+        ArgumentCaptor<String> captorActivity = ArgumentCaptor.forClass(String.class);
+        Mockito.verify(orchestrationContext, times(5))
+                .callActivity(captorActivity.capture(), any(), any(),any());
+        assertEquals(CREATE_INSTITUTION_ACTIVITY, captorActivity.getAllValues().get(0));
+        assertEquals(CREATE_ONBOARDING_ACTIVITY, captorActivity.getAllValues().get(1));
+        assertEquals(CREATE_USERS_ACTIVITY, captorActivity.getAllValues().get(2));
+        assertEquals(STORE_ONBOARDING_ACTIVATEDAT, captorActivity.getAllValues().get(3));
+        assertEquals(SEND_MAIL_COMPLETION_ACTIVITY, captorActivity.getAllValues().get(4));
+
+        Mockito.verify(orchestrationContext, times(3))
+                .callSubOrchestrator(eq(ONBOARDINGS_AGGREGATE_ORCHESTRATOR), any(), any());
+
+        Mockito.verify(service, times(1))
+                .updateOnboardingStatus(onboarding.getId(), OnboardingStatus.COMPLETED);
 
         function.onboardingsOrchestrator(orchestrationContext, executionContext);
     }
@@ -492,6 +535,7 @@ public class OnboardingFunctionsTest {
 
         Task task = mock(Task.class);
         when(orchestrationContext.callActivity(any(),any(),any(),any())).thenReturn(task);
+        when(orchestrationContext.callSubOrchestrator(any(),any())).thenReturn(task);
         when(task.await()).thenReturn("example");
         when(orchestrationContext.allOf(anyList())).thenReturn(task);
         return orchestrationContext;
