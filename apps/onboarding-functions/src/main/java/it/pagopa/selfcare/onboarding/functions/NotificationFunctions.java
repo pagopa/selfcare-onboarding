8860b705bff81bdd72da8f85e855fce58cc1e690
empassaro <113031808+empassaro@users.noreply.github.com>
2024-07-09 14:33:20 +0200

    [SELC-5254] feat: changed http route for CountNotifications fn (#386)

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/NotificationFunctions.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/NotificationFunctions.java
@@ -64,7 +64,7 @@ public class NotificationFunctions {
 
         try {
             onboarding = readOnboardingValue(objectMapper, onboardingString);
-            context.getLogger().info(String.format(FORMAT_LOGGER_ONBOARDING_STRING, SEND_ONBOARDING_NOTIFICATION, onboardingString));
+            context.getLogger().info(() -> String.format(FORMAT_LOGGER_ONBOARDING_STRING, SEND_ONBOARDING_NOTIFICATION, onboardingString));
         } catch (Exception ex) {
             context.getLogger().warning(() -> "Error during sendNotifications execution, msg: " + ex.getMessage());
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
@@ -114,7 +114,7 @@ public class NotificationFunctions {
      */
     @FunctionName("CountNotifications")
     public HttpResponseMessage countNotifications(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
+            @HttpTrigger(name = "req", route = "onboardings/notifications/count", methods = {HttpMethod.GET}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("count trigger processed a request");
 
