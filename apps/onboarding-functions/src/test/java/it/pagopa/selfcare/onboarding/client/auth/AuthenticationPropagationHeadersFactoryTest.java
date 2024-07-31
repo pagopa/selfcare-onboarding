446b549ff71f23a211c769aa73c42f2220b2ab72
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-07-31 12:14:11 +0200

    Added unit test

--- apps/onboarding-functions/src/test/java/it/pagopa/selfcare/onboarding/client/auth/AuthenticationPropagationHeadersFactoryTest.java
+++ apps/onboarding-functions/src/test/java/it/pagopa/selfcare/onboarding/client/auth/AuthenticationPropagationHeadersFactoryTest.java
@@ -1,6 +1,8 @@
 package it.pagopa.selfcare.onboarding.client.auth;
 
+import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
+import it.pagopa.selfcare.onboarding.service.JwtSessionService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MultivaluedHashMap;
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,8 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @QuarkusTest
 class AuthenticationPropagationHeadersFactoryTest {
@@ -16,11 +20,24 @@ class AuthenticationPropagationHeadersFactoryTest {
     @Inject
     AuthenticationPropagationHeadersFactory authenticationPropagationHeadersFactory;
 
+    @InjectMock
+    JwtSessionService jwtSessionService;
+
     @Test
     void update() {
         MultivaluedHashMap<String, String> incomingHeaders = new MultivaluedHashMap<>();
         MultivaluedHashMap<String, String> outgoingHeaders = new MultivaluedHashMap<>();
         outgoingHeaders.put("user-uuid", List.of(UUID.randomUUID().toString()));
+        authenticationPropagationHeadersFactory.update(incomingHeaders, outgoingHeaders);
+        assertTrue(outgoingHeaders.containsKey("Authorization"));
+    }
+
+    @Test
+    void updateWithNullJwt() {
+        MultivaluedHashMap<String, String> incomingHeaders = new MultivaluedHashMap<>();
+        MultivaluedHashMap<String, String> outgoingHeaders = new MultivaluedHashMap<>();
+        outgoingHeaders.put("user-uuid", List.of(UUID.randomUUID().toString()));
+        when(jwtSessionService.createJwt(any())).thenReturn(null);
         authenticationPropagationHeadersFactory.update(incomingHeaders, outgoingHeaders);
         assertTrue(outgoingHeaders.containsKey("Authorization"));
     }
