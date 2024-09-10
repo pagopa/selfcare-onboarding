b5b5ae8da0ef26044269260108c43de6708f244b
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2024-09-10 14:44:12 +0200

    Fixed duplicated msg and added slf4j

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/exception/handler/ExceptionHandler.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/exception/handler/ExceptionHandler.java
@@ -7,22 +7,21 @@ import it.pagopa.selfcare.onboarding.exception.ResourceNotFoundException;
 import it.pagopa.selfcare.onboarding.exception.model.Problem;
 import it.pagopa.selfcare.onboarding.exception.model.ProblemError;
 import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-
+@Slf4j
 public class ExceptionHandler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandler.class);
     public static final String SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER = "Something has gone wrong in the server";
+    public static final String LOG_ERROR_SYNTAX = "{}: {}";
 
     @ServerExceptionMapper
     public Response toResponse(InvalidRequestException exception) {
-        LOGGER.error("{}: {}", SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
+        log.error(LOG_ERROR_SYNTAX, SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
         Problem problem = problem(exception.getMessage(), Response.Status.BAD_REQUEST.getStatusCode(), exception.getCode());
         return Response
                 .status(Response.Status.BAD_REQUEST)
@@ -31,23 +30,23 @@ public class ExceptionHandler {
     }
     @ServerExceptionMapper
     public RestResponse<String> toResponse(OnboardingNotAllowedException exception) {
-        LOGGER.error("{}: {}", SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
+        log.error(LOG_ERROR_SYNTAX, SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
         return RestResponse.status(Response.Status.BAD_REQUEST, exception.getMessage());
     }
 
     @ServerExceptionMapper
     public RestResponse<String> toResponse(Exception exception) {
-        LOGGER.error("{}: {}", SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
+        log.error(LOG_ERROR_SYNTAX, SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
         return RestResponse.status(Response.Status.INTERNAL_SERVER_ERROR, SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER);
     }
     @ServerExceptionMapper
     public RestResponse<String> toResponse(ResourceNotFoundException exception) {
-        LOGGER.error("{}: {}", SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
+        log.error(LOG_ERROR_SYNTAX, SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
         return RestResponse.status(Response.Status.NOT_FOUND, exception.getMessage());
     }
     @ServerExceptionMapper
     public RestResponse<String> toResponse(ResourceConflictException exception) {
-        LOGGER.error("{}: {}", SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
+        log.error(LOG_ERROR_SYNTAX, SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
         return RestResponse.status(Response.Status.CONFLICT, exception.getMessage());
     }
 
