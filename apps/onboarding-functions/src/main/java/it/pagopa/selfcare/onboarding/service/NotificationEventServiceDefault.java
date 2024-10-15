ae6392ca2961bcb2335d5480bc905e4b06ef3786
andrea putzu <andrea.putzu@pagopa.it>
2024-10-15 23:17:11 +0200

    chore: Set createdAt as string

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/service/NotificationEventServiceDefault.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/service/NotificationEventServiceDefault.java
@@ -129,14 +129,16 @@ public class NotificationEventServiceDefault implements NotificationEventService
     private void prepareAndSendUserNotification(ExecutionContext context, Product product, NotificationConfig.Consumer consumer, NotificationsResources notificationsResources, String notificationEventTraceId) {
         NotificationUserBuilder notificationUserBuilder = notificationUserBuilderFactory.create(consumer);
         if (notificationUserBuilder.shouldSendUserNotification(notificationsResources.getOnboarding(), notificationsResources.getInstitution())) {
-            context.getLogger().info(() -> String.format("prepareAndSendUserNotification %s [%s]", notificationsResources.getInstitution().getDescription(), consumer.topic()));
+            context.getLogger().info(() -> String.format("[APZ] prepareAndSendUserNotification %s [%s]", notificationsResources.getInstitution().getDescription(), consumer.topic()));
 
             notificationsResources.getOnboarding().getUsers().forEach(onboardingUser -> {
                 List<UserDataResponse> users = userApi.usersUserIdInstitutionInstitutionIdGet(notificationsResources.getOnboarding().getInstitution().getId(), onboardingUser.getId(), null, null, null, null, null);
                 users.forEach(userDataResponse -> {
-                    context.getLogger().info(() -> String.format("userDataResponse %s [%s]", userDataResponse.getUserId(), consumer.topic()));
+                    context.getLogger().info(() -> String.format("[APZ] userDataResponse %s [%s]", userDataResponse.getUserId(), consumer.topic()));
                     userDataResponse.getProducts().stream().filter(onboardedProductResponse -> {
+                        context.getLogger().info(() -> String.format("[APZ] onboardedProductResponse %s [%s]", onboardedProductResponse.getProductId(), consumer.topic()));
                         if (onboardedProductResponse.getProductId().equals(product.getId())) {
+                            context.getLogger().info(() -> String.format("[APZ] getNotificationUserToSend %s [%s]", userDataResponse.getUserId(), consumer.topic()));
                             NotificationUserToSend notificationUserToSend = getNotificationUserToSend(notificationsResources, userDataResponse, onboardedProductResponse, notificationUserBuilder);
                             context.getLogger().info(() -> String.format("Sending user notification %s", notificationUserToSend.toString()));
                             sendUserNotification(context, consumer.topic(), notificationUserToSend, notificationEventTraceId);
