33a7e2a7b444071c8edf5fb8d5313c00edf0a9c4
andrea putzu <andrea.putzu@pagopa.it>
2024-10-15 22:22:05 +0200

    chore: Adding Logs

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/service/NotificationEventServiceDefault.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/service/NotificationEventServiceDefault.java
@@ -130,19 +130,20 @@ public class NotificationEventServiceDefault implements NotificationEventService
         NotificationUserBuilder notificationUserBuilder = notificationUserBuilderFactory.create(consumer);
         if (notificationUserBuilder.shouldSendUserNotification(notificationsResources.getOnboarding(), notificationsResources.getInstitution())) {
             context.getLogger().info(() -> String.format("prepareAndSendUserNotification %s [%s]", notificationsResources.getInstitution().getDescription(), consumer.topic()));
-            List<UserDataResponse> users = userApi.usersUserIdInstitutionInstitutionIdGet(notificationsResources.getOnboarding().getInstitution().getId(), null, null, null, null, null, null);
-            users.forEach(userDataResponse -> {
-                context.getLogger().info(() -> String.format("userDataResponse %s [%s]", userDataResponse.getUserId(), consumer.topic()));
-                userDataResponse.getProducts().stream().filter(onboardedProductResponse -> {
-                    if (onboardedProductResponse.getProductId().equals(product.getId())) {
-                        NotificationUserToSend notificationUserToSend = getNotificationUserToSend(notificationsResources, userDataResponse, onboardedProductResponse, notificationUserBuilder);
-                        context.getLogger().info(() -> String.format("Sending user notification %s", notificationUserToSend.toString()));
-                        sendUserNotification(context, consumer.topic(), notificationUserToSend, notificationEventTraceId);
-                    }
-                    return false;
+
+            notificationsResources.getOnboarding().getUsers().forEach(onboardingUser -> {
+                List<UserDataResponse> users = userApi.usersUserIdInstitutionInstitutionIdGet(notificationsResources.getOnboarding().getInstitution().getId(), onboardingUser.getId(), null, null, null, null, null);
+                users.forEach(userDataResponse -> {
+                    context.getLogger().info(() -> String.format("userDataResponse %s [%s]", userDataResponse.getUserId(), consumer.topic()));
+                    userDataResponse.getProducts().stream().filter(onboardedProductResponse -> {
+                        if (onboardedProductResponse.getProductId().equals(product.getId())) {
+                            NotificationUserToSend notificationUserToSend = getNotificationUserToSend(notificationsResources, userDataResponse, onboardedProductResponse, notificationUserBuilder);
+                            context.getLogger().info(() -> String.format("Sending user notification %s", notificationUserToSend.toString()));
+                            sendUserNotification(context, consumer.topic(), notificationUserToSend, notificationEventTraceId);
+                        }
+                        return false;
+                    });
                 });
-
-
             });
 
 
