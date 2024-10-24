2e7cba36578c40982daf0ddf45dfe46f5ddc88ec
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2024-10-24 17:36:57 +0200

    Deleted attr contractTemplateUpdatedAt

--- libs/onboarding-sdk-product/src/main/java/it/pagopa/selfcare/product/entity/ContractTemplate.java
+++ libs/onboarding-sdk-product/src/main/java/it/pagopa/selfcare/product/entity/ContractTemplate.java
@@ -1,34 +1,23 @@
 package it.pagopa.selfcare.product.entity;
 
-import java.time.Instant;
-
 public class ContractTemplate {
 
-    private Instant contractTemplateUpdatedAt;
-    private String contractTemplatePath;
-    private String contractTemplateVersion;
+  private String contractTemplatePath;
+  private String contractTemplateVersion;
 
-    public Instant getContractTemplateUpdatedAt() {
-        return contractTemplateUpdatedAt;
-    }
+  public String getContractTemplatePath() {
+    return contractTemplatePath;
+  }
 
-    public void setContractTemplateUpdatedAt(Instant contractTemplateUpdatedAt) {
-        this.contractTemplateUpdatedAt = contractTemplateUpdatedAt;
-    }
+  public void setContractTemplatePath(String contractTemplatePath) {
+    this.contractTemplatePath = contractTemplatePath;
+  }
 
-    public String getContractTemplatePath() {
-        return contractTemplatePath;
-    }
+  public String getContractTemplateVersion() {
+    return contractTemplateVersion;
+  }
 
-    public void setContractTemplatePath(String contractTemplatePath) {
-        this.contractTemplatePath = contractTemplatePath;
-    }
-
-    public String getContractTemplateVersion() {
-        return contractTemplateVersion;
-    }
-
-    public void setContractTemplateVersion(String contractTemplateVersion) {
-        this.contractTemplateVersion = contractTemplateVersion;
-    }
+  public void setContractTemplateVersion(String contractTemplateVersion) {
+    this.contractTemplateVersion = contractTemplateVersion;
+  }
 }
