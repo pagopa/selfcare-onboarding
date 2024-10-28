cb3d524710b6b0e0ee2f25fb556d54ee191e5b6b
Giulia Tremolada <giulia.tremolada@nttdata.com>
2024-10-28 15:10:16 +0100

    add RetrieveAggregates function

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/utils/ActivityName.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/functions/utils/ActivityName.java
@@ -25,6 +25,7 @@ public class ActivityName {
     public static final String CREATE_AGGREGATES_CSV_ACTIVITY = "CreateAggregatesCsv";
     public static final String EXISTS_DELEGATION_ACTIVITY = "ExistsDelegationActivity";
     public static final String DELETE_MANAGERS_BY_IC_AND_ADE = "DeleteManagersByIcAndAde";
+    public static final String RETRIEVE_AGGREGATES_ACTIVITY = "RetrieveAggregates";
 
     private ActivityName() {
     }
