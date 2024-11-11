88912c9f4d78168e2ad82bc4166d94a31ed3b524
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2024-11-11 14:22:42 +0100

    feat: added contractType contractId and providerNames to PaymentServiceProvider of CDC

--- apps/onboarding-cdc/src/main/java/it/pagopa/selfcare/onboarding/event/entity/PaymentServiceProvider.java
+++ apps/onboarding-cdc/src/main/java/it/pagopa/selfcare/onboarding/event/entity/PaymentServiceProvider.java
@@ -2,11 +2,16 @@ package it.pagopa.selfcare.onboarding.event.entity;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class PaymentServiceProvider {
-    private String abiCode;
-    private String businessRegisterNumber;
-    private String legalRegisterNumber;
-    private String legalRegisterName;
-    private boolean vatNumberGroup;
+  private String abiCode;
+  private String businessRegisterNumber;
+  private String legalRegisterNumber;
+  private String legalRegisterName;
+  private boolean vatNumberGroup;
+  private String contractType;
+  private String contractId;
+  private List<String> providerNames;
 }
