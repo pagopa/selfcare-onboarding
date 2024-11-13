1c5074f63e0c91af4d9486d982b8386e520ed93e
andrea putzu <andrea.putzu@pagopa.it>
2024-11-13 13:33:48 +0100

    chore: add slack report

--- test/convert.js
+++ test/convert.js
@@ -1,10 +1,13 @@
+'use strict';
 var fs = require('fs');
+const axios = require('axios').default;
 
 fs.readFile('test/test-result.json', 'utf8', function (err, data) {
     if (err) throw err; // we'll not consider error handling for now
     var obj = JSON.parse(data);
-    
-    let json = JSON.stringify(createBody(obj["run"]["stats"]));
+    sendToSlack(obj["run"]["stats"], process.argv[2]);
+
+    let json = JSON.stringify(result);
     fs.writeFile("test/stats.json", json, 'utf8', (err) => {
       if (err) {
           console.error('Error writing to file', err);
@@ -14,9 +17,19 @@ fs.readFile('test/test-result.json', 'utf8', function (err, data) {
   });
 });
 
+var fs = require('fs');
+// const axios = require('axios').default;
 
-function createBody(payload) {
-    
+fs.readFile('test/test-result.json', 'utf8', function (err, data) {
+    if (err) throw err; 
+    var obj = JSON.parse(data);
+    sendToSlack(obj["run"]["stats"], process.argv[2]);
+});
+
+
+async function sendToSlack(payload, url) {
+    //console.log(payload);
+
     var block =
     {
       "blocks": [
@@ -71,6 +84,15 @@ function createBody(payload) {
       ]
     }
 
-    return block
+    payload = JSON.stringify(block)
+    try {
+     
+      await axios.post(url, payload, {headers: {
+        'Content-Type': 'application/json'
+      }});
+    } catch (e) {
+      const status = e.response.status;
+      console.error(`There was an error, HTTP status code: ${status}`);
+    }
 }
 
