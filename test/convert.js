fbb0cb36d4cfe5fceb723e27df3219136b253e55
andrea putzu <andrea.putzu@pagopa.it>
2024-11-13 10:59:38 +0100

    chore: add slack report

---
+++ test/convert.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+fs.readFile('result/test-result.json', 'utf8', function (err, data) {
+    if (err) throw err; // we'll not consider error handling for now
+    var obj = JSON.parse(data);
+    var run = obj["run"]
+    result = {}
+    result["stats"] = run["stats"]
+    let json = JSON.stringify(result);
+    fs.writeFile("result/result.json", json, 'utf8', (err) => {
+      if (err) {
+          console.error('Error writing to file', err);
+      } else {
+          console.log('Data written to file');
+      }
+  });
+});
