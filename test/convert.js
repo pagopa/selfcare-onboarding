c5cffddf7a57e42d2a274911daa9d609d94078d3
andrea putzu <andrea.putzu@pagopa.it>
2024-11-13 11:11:24 +0100

    chore: add slack report

--- test/convert.js
+++ test/convert.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
 
-fs.readFile('result/test-result.json', 'utf8', function (err, data) {
+fs.readFile('test/test-result.json', 'utf8', function (err, data) {
     if (err) throw err; // we'll not consider error handling for now
     var obj = JSON.parse(data);
     var run = obj["run"]
     result = {}
     result["stats"] = run["stats"]
     let json = JSON.stringify(result);
-    fs.writeFile("result/result.json", json, 'utf8', (err) => {
+    fs.writeFile("test/result.json", json, 'utf8', (err) => {
       if (err) {
           console.error('Error writing to file', err);
       } else {
