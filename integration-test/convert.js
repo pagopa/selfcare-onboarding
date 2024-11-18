0787418059f9490914edb1e85bfd2f9c7bef797b
andrea putzu <andrea.putzu@pagopa.it>
2024-11-18 17:30:40 +0100

    chore: refactor integration test

--- integration-test/convert.js
+++ integration-test/convert.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.readFile('integration-test/test-result.json', 'utf8', function (err, data) {
+fs.readFile('integration-test/integration-test-result.json', 'utf8', function (err, data) {
   if (err) throw err; // we'll not consider error handling for now
   var obj = JSON.parse(data);
   
