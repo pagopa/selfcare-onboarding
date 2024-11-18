3cb3803f6330d57da6574b1bff66405425e15c3e
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2024-11-18 18:10:33 +0100

    Fixed condition on InstitutionUtils

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/util/InstitutionUtils.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/util/InstitutionUtils.java
@@ -11,7 +11,8 @@ public class InstitutionUtils {
   public static String getCurrentInstitutionType(Onboarding onboarding) {
     String institutionType = Product.CONTRACT_TYPE_DEFAULT;
 
-    if (Objects.nonNull(onboarding.getInstitution().getInstitutionType())) {
+    if (Objects.nonNull(onboarding.getInstitution())
+        && Objects.nonNull(onboarding.getInstitution().getInstitutionType())) {
       institutionType = onboarding.getInstitution().getInstitutionType().name();
     }
 
