4b779e0530c0ee19004ec3d0c1d4011a165a9c96
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2024-11-27 09:37:13 +0100

    Added GPUData and BusinessData

---
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/entity/BusinessData.java
@@ -0,0 +1,32 @@
+package it.pagopa.selfcare.onboarding.entity;
+
+public class BusinessData {
+
+    String businessRegisterNumber;
+    String legalRegisterNumber;
+    String legalRegisterName;
+
+    public String getBusinessRegisterNumber() {
+        return businessRegisterNumber;
+    }
+
+    public void setBusinessRegisterNumber(String businessRegisterNumber) {
+        this.businessRegisterNumber = businessRegisterNumber;
+    }
+
+    public String getLegalRegisterNumber() {
+        return legalRegisterNumber;
+    }
+
+    public void setLegalRegisterNumber(String legalRegisterNumber) {
+        this.legalRegisterNumber = legalRegisterNumber;
+    }
+
+    public String getLegalRegisterName() {
+        return legalRegisterName;
+    }
+
+    public void setLegalRegisterName(String legalRegisterName) {
+        this.legalRegisterName = legalRegisterName;
+    }
+}
