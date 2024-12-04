1b296aa56ac6ca9edbf2fd454269df716e02e66b
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-12-04 14:53:03 +0100

    Fixed sonar issue

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/entity/registry/RegistryManagerADE.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/entity/registry/RegistryManagerADE.java
@@ -25,7 +25,7 @@ public class RegistryManagerADE extends ClientRegistryADE {
   @Override
   public Uni<Boolean> isValid() {
     Boolean result = super.registryResource;
-    if (Objects.isNull(result) || !result) {
+    if (Objects.isNull(result) || result == false) {
       return Uni.createFrom()
           .failure(
               new InvalidRequestException(
