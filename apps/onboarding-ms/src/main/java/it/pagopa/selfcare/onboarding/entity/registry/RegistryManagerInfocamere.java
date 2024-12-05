768255bae37143cd429c47869ed02745dc7c5f17
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2024-12-05 16:24:29 +0100

    [SELC-6138] Adding geographicTaxonomies on PNPG institutions (#630)

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/entity/registry/RegistryManagerInfocamere.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/entity/registry/RegistryManagerInfocamere.java
@@ -1,10 +1,15 @@
 package it.pagopa.selfcare.onboarding.entity.registry;
 
 import io.smallrye.mutiny.Uni;
+import it.pagopa.selfcare.onboarding.entity.IPAEntity;
 import it.pagopa.selfcare.onboarding.entity.Onboarding;
 import it.pagopa.selfcare.onboarding.entity.registry.client.ClientRegistryInfocamere;
 import it.pagopa.selfcare.product.entity.Product;
 import org.openapi.quarkus.party_registry_proxy_json.api.InfocamereApi;
+import org.openapi.quarkus.party_registry_proxy_json.model.BusinessesResource;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class RegistryManagerInfocamere extends ClientRegistryInfocamere {
 
@@ -22,4 +27,12 @@ public class RegistryManagerInfocamere extends ClientRegistryInfocamere {
     return Uni.createFrom().item(true);
   }
   
+  @Override
+  public RegistryManager<BusinessesResource> setResource(BusinessesResource registryResource) {
+    this.registryResource = registryResource;
+    if (Objects.isNull(onboarding.getInstitution().getGeographicTaxonomies())) {
+      onboarding.getInstitution().setGeographicTaxonomies(new ArrayList<>());
+    }
+    return this;
+  }
 }
