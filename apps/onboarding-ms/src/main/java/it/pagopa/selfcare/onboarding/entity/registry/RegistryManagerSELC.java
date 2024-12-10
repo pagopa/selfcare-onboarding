609c8f236713f374e8455a16cf4844831ca43b15
empassaro <emanuele.passaro@emeal.nttdata.com>
2024-12-10 11:13:57 +0100

    [SELC-6062] feat: added check on workflow type for SELC Manager and default origin and originId

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/entity/registry/RegistryManagerSELC.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/entity/registry/RegistryManagerSELC.java
@@ -1,7 +1,10 @@
 package it.pagopa.selfcare.onboarding.entity.registry;
 
 import io.smallrye.mutiny.Uni;
+import it.pagopa.selfcare.onboarding.common.Origin;
+import it.pagopa.selfcare.onboarding.common.WorkflowType;
 import it.pagopa.selfcare.onboarding.entity.Onboarding;
+import it.pagopa.selfcare.onboarding.exception.InvalidRequestException;
 import it.pagopa.selfcare.product.entity.Product;
 
 public class RegistryManagerSELC extends BaseRegistryManager<Object> {
@@ -11,12 +14,28 @@ public class RegistryManagerSELC extends BaseRegistryManager<Object> {
     }
 
     public Object retrieveInstitution() {
-      return Uni.createFrom().item(new Object());
+        setDefaultOriginAndOriginId();
+        return Uni.createFrom().item(new Object());
+    }
+
+    private void setDefaultOriginAndOriginId() {
+        onboarding.getInstitution().setOriginId(onboarding.getInstitution().getTaxCode());
+        onboarding.getInstitution().setOrigin(Origin.SELC);
     }
 
     @Override
     public Uni<Onboarding> customValidation(Product product) {
-        return Uni.createFrom().item(onboarding);
+        if (isWorkflowTypeAllowed(onboarding.getWorkflowType())) {
+            return Uni.createFrom().item(onboarding);
+        }
+
+        return Uni.createFrom().failure(new InvalidRequestException("Invalid workflow type for origin SELC"));
+    }
+
+    protected boolean isWorkflowTypeAllowed(WorkflowType workflowType) {
+        return workflowType == WorkflowType.FOR_APPROVE ||
+                workflowType == WorkflowType.IMPORT ||
+                workflowType == WorkflowType.FOR_APPROVE_PT;
     }
 
     @Override
