831df1572bbf0f1c194aadeef25267cd837e1b05
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2024-12-10 17:09:04 +0100

    Fixed controller and endpoints

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/controller/DocumentController.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/controller/DocumentController.java
@@ -7,18 +7,17 @@ import it.pagopa.selfcare.azurestorage.AzureBlobClient;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Authenticated
-@Path("/v1/document")
+@Path("/v1/documents")
 @Tag(name = "Document Controller")
 @AllArgsConstructor
 @Slf4j
@@ -26,6 +25,23 @@ public class DocumentController {
 
     @Inject
     AzureBlobClient blobClient;
+
+    /**
+     * Retrieves the list of files by the path on the blob
+     *
+     * @return List of files on the storage * Code: 200, Message: successful operation * Code: 400, Message: Invalid ID supplied * Code: 404, Message:
+     * Path not found
+     */
+
+    @Operation(
+        summary = "Retrieves the list of files on the azure storage on the given path",
+        description = "Fetches a list of files associated with the specified path on the storage."
+    )
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<List<String>> getFiles() {
+        return Uni.createFrom().item(blobClient.getFiles());
+    }
 
     /**
      * Retrieves the list of files by the path on the blob
@@ -40,11 +56,10 @@ public class DocumentController {
         description = "Fetches a list of files associated with the specified path on the storage."
     )
     @GET
+    @Path("/{path}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/list-file")
-    public Uni<List<String>> getFiles(@QueryParam(value = "path") String path) {
-        String sanitizePath = StringUtils.replace(path, "\n", StringUtils.EMPTY).replace("\r", StringUtils.EMPTY);
-        return Uni.createFrom().item(blobClient.getFiles(sanitizePath));
+    public Uni<List<String>> getFiles(@PathParam(value = "path") String path) {
+        return Uni.createFrom().item(blobClient.getFiles(path));
     }
 
 }
