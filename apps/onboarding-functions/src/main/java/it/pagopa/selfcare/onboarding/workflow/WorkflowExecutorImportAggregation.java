1e49b333290d2b4f7e6f74b3d80e9f29fe64e3e7
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2025-01-20 11:59:13 +0100

    Added new workflow execution for Import Aggregation

---
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/workflow/WorkflowExecutorImportAggregation.java
@@ -0,0 +1,35 @@
+package it.pagopa.selfcare.onboarding.workflow;
+
+import static it.pagopa.selfcare.onboarding.entity.OnboardingWorkflowType.INSTITUTION;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.durabletask.TaskOptions;
+import com.microsoft.durabletask.TaskOrchestrationContext;
+import it.pagopa.selfcare.onboarding.common.OnboardingStatus;
+import it.pagopa.selfcare.onboarding.entity.Onboarding;
+import it.pagopa.selfcare.onboarding.entity.OnboardingWorkflow;
+import it.pagopa.selfcare.onboarding.entity.OnboardingWorkflowInstitution;
+import java.util.Optional;
+
+public record WorkflowExecutorImportAggregation(ObjectMapper objectMapper, TaskOptions optionsRetry) implements WorkflowExecutor {
+
+    @Override
+    public Optional<OnboardingStatus> executeRequestState(TaskOrchestrationContext ctx, OnboardingWorkflow onboardingWorkflow) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<OnboardingStatus> executeToBeValidatedState(TaskOrchestrationContext ctx, OnboardingWorkflow onboardingWorkflow) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<OnboardingStatus> executePendingState(TaskOrchestrationContext ctx, OnboardingWorkflow onboardingWorkflow) {
+        return onboardingCompletionActivityWithoutMail(ctx, onboardingWorkflow.getOnboarding());
+    }
+
+    @Override
+    public OnboardingWorkflow createOnboardingWorkflow(Onboarding onboarding) {
+        return new OnboardingWorkflowInstitution(onboarding, INSTITUTION.name());
+    }
+}
