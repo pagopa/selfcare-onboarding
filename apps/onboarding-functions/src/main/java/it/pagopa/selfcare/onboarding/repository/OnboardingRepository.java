50d17985754f0b56d05c3d10cafb1e614036c6d9
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2025-01-22 17:14:08 +0100

    Added check for previous manager in case of new user onboarding

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/repository/OnboardingRepository.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/repository/OnboardingRepository.java
@@ -1,10 +1,24 @@
 package it.pagopa.selfcare.onboarding.repository;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepositoryBase;
+import io.smallrye.mutiny.Multi;
+import it.pagopa.selfcare.onboarding.common.OnboardingStatus;
 import it.pagopa.selfcare.onboarding.entity.Onboarding;
+import it.pagopa.selfcare.onboarding.utils.QueryUtils;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.bson.Document;
+
+import java.util.List;
+import java.util.Map;
 
 @ApplicationScoped
 public class OnboardingRepository implements PanacheMongoRepositoryBase<Onboarding, String> {
 
+    public List<Onboarding> findByFilters(String taxCode, String subunitCode, String origin, String originId, String productId) {
+        final Map<String, String> queryParameter =
+                QueryUtils.createMapForInstitutionOnboardingsQueryParameter(
+                        taxCode, subunitCode, origin, originId, OnboardingStatus.COMPLETED, productId);
+        Document query = QueryUtils.buildQuery(queryParameter);
+        return find(query).list();
+    }
 }
