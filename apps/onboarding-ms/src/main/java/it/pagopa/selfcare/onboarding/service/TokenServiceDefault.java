591f11c41c33ec72fad21fda4c06b581472c73c0
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2025-01-22 12:59:15 +0100

    [SELC-6341] Adding new API for check if contractSigned file is CADES (#680)

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/service/TokenServiceDefault.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/service/TokenServiceDefault.java
@@ -1,102 +1,121 @@
 package it.pagopa.selfcare.onboarding.service;
 
 import static it.pagopa.selfcare.onboarding.common.TokenType.ATTACHMENT;
-import static it.pagopa.selfcare.onboarding.common.TokenType.INSTITUTION;
 
 import io.smallrye.mutiny.Uni;
 import it.pagopa.selfcare.azurestorage.AzureBlobClient;
 import it.pagopa.selfcare.onboarding.conf.OnboardingMsConfig;
+import it.pagopa.selfcare.onboarding.controller.response.ContractSignedReport;
 import it.pagopa.selfcare.onboarding.entity.Token;
 import it.pagopa.selfcare.onboarding.exception.InvalidRequestException;
 import it.pagopa.selfcare.onboarding.util.QueryUtils;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MediaType;
+
 import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
+
 import org.bson.Document;
 import org.jboss.resteasy.reactive.RestResponse;
 
 @ApplicationScoped
 public class TokenServiceDefault implements TokenService {
-    private final AzureBlobClient azureBlobClient;
-    private final OnboardingMsConfig onboardingMsConfig;
+  @Inject
+  SignatureService signatureService;
 
-    private static final String ONBOARDING_NOT_FOUND_OR_ALREADY_DELETED =
-        "Token with id %s not found or already deleted";
+  private final AzureBlobClient azureBlobClient;
+  private final OnboardingMsConfig onboardingMsConfig;
 
-    public TokenServiceDefault(AzureBlobClient azureBlobClient, OnboardingMsConfig onboardingMsConfig) {
-        this.azureBlobClient = azureBlobClient;
-        this.onboardingMsConfig = onboardingMsConfig;
-    }
+  private static final String ONBOARDING_NOT_FOUND_OR_ALREADY_DELETED =
+    "Token with id %s not found or already deleted";
 
-    @Override
-    public Uni<List<Token>> getToken(String onboardingId) {
-        return Token.find("onboardingId", onboardingId)
-                .list();
-    }
-    @Override
-    public Uni<RestResponse<File>> retrieveContractNotSigned(String onboardingId) {
-        return Token.findById(onboardingId)
-                .map(Token.class::cast)
-                .onItem().transformToUni(token ->
-                            Uni.createFrom().item(() -> azureBlobClient.getFileAsPdf(String.format("%s%s/%s", onboardingMsConfig.getContractPath(), onboardingId, token.getContractFilename())))
-                                    .runSubscriptionOn(Executors.newSingleThreadExecutor())
-                                    .onItem().transform(contract -> {
-                                        RestResponse.ResponseBuilder<File> response = RestResponse.ResponseBuilder.ok(contract, MediaType.APPLICATION_OCTET_STREAM);
-                                        response.header("Content-Disposition", "attachment;filename=" + token.getContractFilename());
-                                        return response.build();
-                                    }));
-    }
+  public TokenServiceDefault(AzureBlobClient azureBlobClient, OnboardingMsConfig onboardingMsConfig) {
+    this.azureBlobClient = azureBlobClient;
+    this.onboardingMsConfig = onboardingMsConfig;
+  }
 
-    @Override
-    public Uni<RestResponse<File>> retrieveAttachment(String onboardingId, String attachmentName) {
-        return Token.find("onboardingId = ?1 and type = ?2 and name = ?3", onboardingId, ATTACHMENT.name(), attachmentName)
-                .firstResult()
-                .map(Token.class::cast)
-                .onItem().transformToUni(token ->
-                        Uni.createFrom().item(() -> azureBlobClient.getFileAsPdf(getAttachmentByOnboarding(onboardingId, token.getContractFilename())))
-                                .runSubscriptionOn(Executors.newSingleThreadExecutor())
-                                .onItem().transform(contract -> {
-                                    RestResponse.ResponseBuilder<File> response = RestResponse.ResponseBuilder.ok(contract, MediaType.APPLICATION_OCTET_STREAM);
-                                    response.header("Content-Disposition", "attachment;filename=" + token.getContractFilename());
-                                    return response.build();
-                                }));
-    }
+  @Override
+  public Uni<List<Token>> getToken(String onboardingId) {
+    return Token.find("onboardingId", onboardingId)
+      .list();
+  }
 
-    @Override
-    public Uni<Long> updateContractSigned(String onboardingId, String documentSignedPath) {
+  @Override
+  public Uni<RestResponse<File>> retrieveContractNotSigned(String onboardingId) {
+    return Token.findById(onboardingId)
+      .map(Token.class::cast)
+      .onItem().transformToUni(token ->
+        Uni.createFrom().item(() -> azureBlobClient.getFileAsPdf(String.format("%s%s/%s", onboardingMsConfig.getContractPath(), onboardingId, token.getContractFilename())))
+          .runSubscriptionOn(Executors.newSingleThreadExecutor())
+          .onItem().transform(contract -> {
+            RestResponse.ResponseBuilder<File> response = RestResponse.ResponseBuilder.ok(contract, MediaType.APPLICATION_OCTET_STREAM);
+            response.header("Content-Disposition", "attachment;filename=" + token.getContractFilename());
+            return response.build();
+          }));
+  }
 
-        Map<String, Object> queryParameter = new HashMap<>();
-        queryParameter.put("contractSigned", documentSignedPath);
-        Document query = QueryUtils.buildUpdateDocument(queryParameter);
+  @Override
+  public Uni<RestResponse<File>> retrieveAttachment(String onboardingId, String attachmentName) {
+    return Token.find("onboardingId = ?1 and type = ?2 and name = ?3", onboardingId, ATTACHMENT.name(), attachmentName)
+      .firstResult()
+      .map(Token.class::cast)
+      .onItem().transformToUni(token ->
+        Uni.createFrom().item(() -> azureBlobClient.getFileAsPdf(getAttachmentByOnboarding(onboardingId, token.getContractFilename())))
+          .runSubscriptionOn(Executors.newSingleThreadExecutor())
+          .onItem().transform(contract -> {
+            RestResponse.ResponseBuilder<File> response = RestResponse.ResponseBuilder.ok(contract, MediaType.APPLICATION_OCTET_STREAM);
+            response.header("Content-Disposition", "attachment;filename=" + token.getContractFilename());
+            return response.build();
+          }));
+  }
 
-        return Token.update(query)
-            .where("_id", onboardingId)
-            .onItem()
-            .transformToUni(
-                updateItemCount -> {
-                    if (updateItemCount == 0) {
-                        return Uni.createFrom()
-                            .failure(
-                                new InvalidRequestException(
-                                    String.format(ONBOARDING_NOT_FOUND_OR_ALREADY_DELETED, onboardingId)));
-                    }
-                    return Uni.createFrom().item(updateItemCount);
-                });
-    }
+  @Override
+  public Uni<Long> updateContractSigned(String onboardingId, String documentSignedPath) {
 
-    public Uni<List<String>> getAttachments(String onboardingId) {
-        return Token.find("onboardingId = ?1 and type = ?2", onboardingId, ATTACHMENT.name())
-            .stream().onItem().transform(Token.class::cast)
-            .map(Token::getName)
-            .collect().asList();
-    }
+    Map<String, Object> queryParameter = new HashMap<>();
+    queryParameter.put("contractSigned", documentSignedPath);
+    Document query = QueryUtils.buildUpdateDocument(queryParameter);
 
-    private String getAttachmentByOnboarding(String onboardingId, String filename) {
-        return String.format("%s%s%s%s", onboardingMsConfig.getContractPath(), onboardingId, "/attachments", "/" + filename);
-    }
+    return Token.update(query)
+      .where("_id", onboardingId)
+      .onItem()
+      .transformToUni(
+        updateItemCount -> {
+          if (updateItemCount == 0) {
+            return Uni.createFrom()
+              .failure(
+                new InvalidRequestException(
+                  String.format(ONBOARDING_NOT_FOUND_OR_ALREADY_DELETED, onboardingId)));
+          }
+          return Uni.createFrom().item(updateItemCount);
+        });
+  }
 
+  public Uni<List<String>> getAttachments(String onboardingId) {
+    return Token.find("onboardingId = ?1 and type = ?2", onboardingId, ATTACHMENT.name())
+      .stream().onItem().transform(Token.class::cast)
+      .map(Token::getName)
+      .collect().asList();
+  }
+
+  private String getAttachmentByOnboarding(String onboardingId, String filename) {
+    return String.format("%s%s%s%s", onboardingMsConfig.getContractPath(), onboardingId, "/attachments", "/" + filename);
+  }
+
+  @Override
+  public Uni<ContractSignedReport> reportContractSigned(String onboardingId) {
+    return Token.findById(onboardingId)
+      .map(Token.class::cast)
+      .onItem().transformToUni(token ->
+        Uni.createFrom().item(() -> azureBlobClient.getFileAsPdf(token.getContractSigned()))
+          .runSubscriptionOn(Executors.newSingleThreadExecutor())
+          .onItem().transform(contract -> {
+            signatureService.verifySignature(contract);
+            return ContractSignedReport.cades(true);
+          })).onFailure().recoverWithUni(() -> Uni.createFrom().item(ContractSignedReport.cades(false)));
+  }
 }
