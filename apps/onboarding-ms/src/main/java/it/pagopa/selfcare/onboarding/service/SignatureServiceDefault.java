144fc82b7df3644b56c60f621da9b7b022b02f46
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2025-01-22 12:59:15 +0100

    [SELC-6341] Adding new API for check if contractSigned file is CADES (#680)
    (cherry picked from commit 591f11c41c33ec72fad21fda4c06b581472c73c0)

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/service/SignatureServiceDefault.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/service/SignatureServiceDefault.java
@@ -35,173 +35,204 @@ import static it.pagopa.selfcare.onboarding.util.ErrorMessage.*;
 @ApplicationScoped
 public class SignatureServiceDefault implements SignatureService {
 
-    private static final Integer CF_MATCHER_GROUP = 2;
-    private static final Pattern signatureRegex = Pattern.compile("(TINIT-)(.*)");
+  private static final Integer CF_MATCHER_GROUP = 2;
+  private static final Pattern signatureRegex = Pattern.compile("(TINIT-)(.*)");
 
-    private final TrustedListsCertificateSource trustedListsCertificateSource;
+  private final TrustedListsCertificateSource trustedListsCertificateSource;
 
-    public SignatureServiceDefault(TrustedListsCertificateSource trustedListsCertificateSource) {
-        this.trustedListsCertificateSource = trustedListsCertificateSource;
+  public SignatureServiceDefault(TrustedListsCertificateSource trustedListsCertificateSource) {
+    this.trustedListsCertificateSource = trustedListsCertificateSource;
+  }
+
+
+  public SignedDocumentValidator createDocumentValidator(byte[] bytes) {
+
+    CertificateVerifier certificateVerifier = new CommonCertificateVerifier();
+    certificateVerifier.setTrustedCertSources(trustedListsCertificateSource);
+    certificateVerifier.setAIASource(new DefaultAIASource());
+    certificateVerifier.setOcspSource(new OnlineOCSPSource());
+    certificateVerifier.setCrlSource(new OnlineCRLSource());
+    try {
+      DSSDocument document = new InMemoryDocument(bytes);
+      SignedDocumentValidator documentValidator = SignedDocumentValidator.fromDocument(document);
+      documentValidator.setCertificateVerifier(certificateVerifier);
+      return documentValidator;
+    } catch (Exception e) {
+      log.error("Error message: {}", e.getMessage(), e);
+      throw new InvalidRequestException(DOCUMENT_VALIDATION_FAIL.getMessage(), DOCUMENT_VALIDATION_FAIL.getCode());
+    }
+  }
+
+  public void isDocumentSigned(SignedDocumentValidator documentValidator) {
+    if (documentValidator.getSignatures().isEmpty()) {
+      throw new InvalidRequestException(SIGNATURE_VALIDATION_ERROR.getMessage(), SIGNATURE_VALIDATION_ERROR.getCode());
+    }
+  }
+
+  public void verifyOriginalDocument(SignedDocumentValidator validator) {
+    List<AdvancedSignature> advancedSignatures = validator.getSignatures();
+    List<DSSDocument> dssDocuments = new ArrayList<>();
+    if (advancedSignatures != null && !advancedSignatures.isEmpty()) {
+      for (AdvancedSignature a : advancedSignatures) {
+        Optional.ofNullable(validator.getOriginalDocuments(a.getId())).ifPresent(dssDocuments::addAll);
+      }
+    }
+    if (dssDocuments.isEmpty()) {
+      throw new InvalidRequestException(ORIGINAL_DOCUMENT_NOT_FOUND.getMessage(), ORIGINAL_DOCUMENT_NOT_FOUND.getCode());
+    }
+  }
+
+  public Reports validateDocument(SignedDocumentValidator signedDocumentValidator) {
+    try {
+      return signedDocumentValidator.validateDocument();
+    } catch (Exception e) {
+      throw new InvalidRequestException(String.format(DOCUMENT_VALIDATION_FAIL.getMessage(), e.getMessage()), DOCUMENT_VALIDATION_FAIL.getCode());
+    }
+  }
+
+  public void verifySignatureForm(SignedDocumentValidator validator) {
+
+    String signatureFormErrors = validator.getSignatures().stream()
+      .map(AdvancedSignature::getSignatureForm)
+      .filter(signatureForm -> signatureForm != SignatureForm.CAdES)
+      .map(SignatureForm::toString)
+      .collect(Collectors.joining(","));
+
+    if (!StringUtils.isBlank(signatureFormErrors)) {
+      throw new InvalidRequestException(String.format(INVALID_SIGNATURE_FORMS.getMessage(), signatureFormErrors),
+        INVALID_SIGNATURE_FORMS.getCode());
+    }
+  }
+
+  public void verifySignature(Reports reports) {
+    List<SignatureValidationReportType> signatureValidationReportTypes = new ArrayList<>();
+
+    if (reports.getEtsiValidationReportJaxb() != null) {
+      signatureValidationReportTypes = reports.getEtsiValidationReportJaxb().getSignatureValidationReport();
+    }
+    if (signatureValidationReportTypes.isEmpty()
+      || (!signatureValidationReportTypes.stream().allMatch(s -> s.getSignatureValidationStatus() != null
+      && Indication.TOTAL_PASSED == s.getSignatureValidationStatus().getMainIndication()))) {
+      throw new InvalidRequestException(INVALID_DOCUMENT_SIGNATURE.getMessage(), INVALID_DOCUMENT_SIGNATURE.getCode());
+    }
+  }
+
+  public void checkSignature(Reports reports) {
+    List<SignatureValidationReportType> signatureValidationReportTypes = new ArrayList<>();
+
+    if (reports.getEtsiValidationReportJaxb() != null) {
+      signatureValidationReportTypes = reports.getEtsiValidationReportJaxb().getSignatureValidationReport();
+    }
+    if (signatureValidationReportTypes.isEmpty()
+      || (!signatureValidationReportTypes.stream().allMatch(s -> s.getSignatureValidationStatus() != null))) {
+      throw new InvalidRequestException(INVALID_DOCUMENT_SIGNATURE.getMessage(), INVALID_DOCUMENT_SIGNATURE.getCode());
+    }
+  }
+
+  public void verifyDigest(SignedDocumentValidator validator, String checksum) {
+    List<AdvancedSignature> advancedSignatures = validator.getSignatures();
+
+    if (advancedSignatures != null && !advancedSignatures.isEmpty()) {
+      for (AdvancedSignature a : advancedSignatures) {
+
+        List<DSSDocument> dssDocuments = validator.getOriginalDocuments(a.getId());
+        if (!dssDocuments.stream().map(dssDocument -> dssDocument.getDigest(DigestAlgorithm.SHA256))
+          .collect(Collectors.toList()).contains(checksum)) {
+          throw new InvalidRequestException(INVALID_CONTRACT_DIGEST.getMessage(), INVALID_CONTRACT_DIGEST.getCode());
+        }
+      }
     }
 
+  }
 
-    public SignedDocumentValidator createDocumentValidator(byte[] bytes) {
+  @Override
+  public void verifySignature(File file, String checksum, List<String> usersTaxCode) {
+    try {
+      byte[] byteData = Files.readAllBytes(file.toPath());
 
-        CertificateVerifier certificateVerifier = new CommonCertificateVerifier();
-        certificateVerifier.setTrustedCertSources(trustedListsCertificateSource);
-        certificateVerifier.setAIASource(new DefaultAIASource());
-        certificateVerifier.setOcspSource(new OnlineOCSPSource());
-        certificateVerifier.setCrlSource(new OnlineCRLSource());
-        try {
-            DSSDocument document = new InMemoryDocument(bytes);
-            SignedDocumentValidator documentValidator = SignedDocumentValidator.fromDocument(document);
-            documentValidator.setCertificateVerifier(certificateVerifier);
-            return documentValidator;
-        } catch (Exception e) {
-            log.error("Error message: {}", e.getMessage(), e);
-            throw new InvalidRequestException(DOCUMENT_VALIDATION_FAIL.getMessage(), DOCUMENT_VALIDATION_FAIL.getCode());
-        }
+      SignedDocumentValidator validator = createDocumentValidator(byteData);
+      isDocumentSigned(validator);
+      verifyOriginalDocument(validator);
+      Reports reports = validateDocument(validator);
+
+      verifySignatureForm(validator);
+      verifySignature(reports);
+      verifyDigest(validator, checksum);
+      verifyManagerTaxCode(reports, usersTaxCode);
+
+    } catch (InvalidRequestException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new InvalidRequestException(GENERIC_ERROR.getMessage(), GENERIC_ERROR.getCode());
+    }
+  }
+
+  @Override
+  public void verifySignature(File file) {
+    try {
+      byte[] byteData = Files.readAllBytes(file.toPath());
+
+      SignedDocumentValidator validator = createDocumentValidator(byteData);
+      isDocumentSigned(validator);
+      verifyOriginalDocument(validator);
+      Reports reports = validateDocument(validator);
+
+      verifySignatureForm(validator);
+      checkSignature(reports);
+    } catch (InvalidRequestException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new InvalidRequestException(GENERIC_ERROR.getMessage(), GENERIC_ERROR.getCode());
+    }
+  }
+
+  public void verifyManagerTaxCode(Reports reports, List<String> usersTaxCode) {
+    List<String> signatureTaxCodes = extractSubjectSNCFs(reports);
+    if (signatureTaxCodes.isEmpty()) {
+      throw new InvalidRequestException(TAX_CODE_NOT_FOUND_IN_SIGNATURE.getMessage(), TAX_CODE_NOT_FOUND_IN_SIGNATURE.getCode());
     }
 
-    public void isDocumentSigned(SignedDocumentValidator documentValidator) {
-        if (documentValidator.getSignatures().isEmpty()) {
-            throw new InvalidRequestException(SIGNATURE_VALIDATION_ERROR.getMessage(), SIGNATURE_VALIDATION_ERROR.getCode());
-        }
+    List<String> taxCodes = extractTaxCode(signatureTaxCodes);
+
+    if (taxCodes.isEmpty() || !isSignedByLegal(usersTaxCode, taxCodes)) {
+      throw new InvalidRequestException(INVALID_SIGNATURE_TAX_CODE.getMessage(), INVALID_SIGNATURE_TAX_CODE.getCode());
     }
 
-    public void verifyOriginalDocument(SignedDocumentValidator validator) {
-        List<AdvancedSignature> advancedSignatures = validator.getSignatures();
-        List<DSSDocument> dssDocuments = new ArrayList<>();
-        if (advancedSignatures != null && !advancedSignatures.isEmpty()) {
-            for (AdvancedSignature a : advancedSignatures) {
-                Optional.ofNullable(validator.getOriginalDocuments(a.getId())).ifPresent(dssDocuments::addAll);
-            }
-        }
-        if (dssDocuments.isEmpty()) {
-            throw new InvalidRequestException(ORIGINAL_DOCUMENT_NOT_FOUND.getMessage(), ORIGINAL_DOCUMENT_NOT_FOUND.getCode());
-        }
+  }
+
+  private List<String> extractTaxCode(List<String> signatureTaxCodes) {
+    List<String> taxCode = new ArrayList<>();
+    signatureTaxCodes.forEach(s -> {
+      Matcher matcher = signatureRegex.matcher(s);
+      if (matcher.matches()) {
+        taxCode.add(matcher.group(CF_MATCHER_GROUP));
+      }
+    });
+    return taxCode;
+  }
+
+  private boolean isSignedByLegal(List<String> usersTaxCode, List<String> signatureTaxCodes) {
+    return !signatureTaxCodes.isEmpty() && !usersTaxCode.isEmpty()
+      && new HashSet<>(signatureTaxCodes).containsAll(usersTaxCode);
+  }
+
+  private List<String> extractSubjectSNCFs(Reports reports) {
+    if (reports.getDiagnosticData() != null && reports.getDiagnosticData().getUsedCertificates() != null) {
+      List<String> subjectSNCFs = reports.getDiagnosticData().getUsedCertificates()
+        .stream().map(CertificateWrapper::getSubjectSerialNumber)
+        .filter(this::serialNumberMatch).collect(Collectors.toList());
+      if (!subjectSNCFs.isEmpty()) {
+        return subjectSNCFs;
+      }
     }
+    return Collections.emptyList();
+  }
 
-    public Reports validateDocument(SignedDocumentValidator signedDocumentValidator) {
-        try {
-            return signedDocumentValidator.validateDocument();
-        } catch (Exception e) {
-            throw new InvalidRequestException(String.format(DOCUMENT_VALIDATION_FAIL.getMessage(), e.getMessage()), DOCUMENT_VALIDATION_FAIL.getCode());
-        }
+  private boolean serialNumberMatch(String s) {
+    if (!StringUtils.isEmpty(s)) {
+      return signatureRegex.matcher(s).matches();
     }
-
-    public void verifySignatureForm(SignedDocumentValidator validator) {
-
-        String signatureFormErrors = validator.getSignatures().stream()
-                .map(AdvancedSignature::getSignatureForm)
-                .filter(signatureForm -> signatureForm != SignatureForm.CAdES)
-                .map(SignatureForm::toString)
-                .collect(Collectors.joining(","));
-
-        if (!StringUtils.isBlank(signatureFormErrors)) {
-            throw new InvalidRequestException(String.format(INVALID_SIGNATURE_FORMS.getMessage(), signatureFormErrors),
-                    INVALID_SIGNATURE_FORMS.getCode());
-        }
-    }
-
-    public void verifySignature(Reports reports) {
-        List<SignatureValidationReportType> signatureValidationReportTypes = new ArrayList<>();
-
-        if (reports.getEtsiValidationReportJaxb() != null) {
-            signatureValidationReportTypes = reports.getEtsiValidationReportJaxb().getSignatureValidationReport();
-        }
-        if (signatureValidationReportTypes.isEmpty()
-                || (!signatureValidationReportTypes.stream().allMatch(s -> s.getSignatureValidationStatus() != null
-                && Indication.TOTAL_PASSED == s.getSignatureValidationStatus().getMainIndication()))) {
-            throw new InvalidRequestException(INVALID_DOCUMENT_SIGNATURE.getMessage(), INVALID_DOCUMENT_SIGNATURE.getCode());
-        }
-    }
-
-    public void verifyDigest(SignedDocumentValidator validator, String checksum) {
-        List<AdvancedSignature> advancedSignatures = validator.getSignatures();
-
-        if (advancedSignatures != null && !advancedSignatures.isEmpty()) {
-            for (AdvancedSignature a : advancedSignatures) {
-
-                List<DSSDocument> dssDocuments = validator.getOriginalDocuments(a.getId());
-                if (!dssDocuments.stream().map(dssDocument -> dssDocument.getDigest(DigestAlgorithm.SHA256))
-                        .collect(Collectors.toList()).contains(checksum)) {
-                    throw new InvalidRequestException(INVALID_CONTRACT_DIGEST.getMessage(), INVALID_CONTRACT_DIGEST.getCode());
-                }
-            }
-        }
-
-    }
-
-    @Override
-    public void verifySignature(File file, String checksum, List<String> usersTaxCode) {
-        try {
-            byte[] byteData = Files.readAllBytes(file.toPath());
-
-            SignedDocumentValidator validator = createDocumentValidator(byteData);
-            isDocumentSigned(validator);
-            verifyOriginalDocument(validator);
-            Reports reports = validateDocument(validator);
-
-            verifySignatureForm(validator);
-            verifySignature(reports);
-            verifyDigest(validator, checksum);
-            verifyManagerTaxCode(reports, usersTaxCode);
-
-        } catch (InvalidRequestException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new InvalidRequestException(GENERIC_ERROR.getMessage(), GENERIC_ERROR.getCode());
-        }
-    }
-
-    public void verifyManagerTaxCode(Reports reports, List<String> usersTaxCode) {
-        List<String> signatureTaxCodes = extractSubjectSNCFs(reports);
-        if (signatureTaxCodes.isEmpty()) {
-            throw new InvalidRequestException(TAX_CODE_NOT_FOUND_IN_SIGNATURE.getMessage(), TAX_CODE_NOT_FOUND_IN_SIGNATURE.getCode());
-        }
-
-        List<String> taxCodes = extractTaxCode(signatureTaxCodes);
-
-        if (taxCodes.isEmpty() || !isSignedByLegal(usersTaxCode, taxCodes)) {
-            throw new InvalidRequestException(INVALID_SIGNATURE_TAX_CODE.getMessage(), INVALID_SIGNATURE_TAX_CODE.getCode());
-        }
-
-    }
-
-    private List<String> extractTaxCode(List<String> signatureTaxCodes) {
-        List<String> taxCode = new ArrayList<>();
-        signatureTaxCodes.forEach(s -> {
-            Matcher matcher = signatureRegex.matcher(s);
-            if (matcher.matches()) {
-                taxCode.add(matcher.group(CF_MATCHER_GROUP));
-            }
-        });
-        return taxCode;
-    }
-
-    private boolean isSignedByLegal(List<String> usersTaxCode, List<String> signatureTaxCodes) {
-        return !signatureTaxCodes.isEmpty() && !usersTaxCode.isEmpty()
-                && new HashSet<>(signatureTaxCodes).containsAll(usersTaxCode);
-    }
-
-    private List<String> extractSubjectSNCFs(Reports reports) {
-        if (reports.getDiagnosticData() != null && reports.getDiagnosticData().getUsedCertificates() != null) {
-            List<String> subjectSNCFs = reports.getDiagnosticData().getUsedCertificates()
-                    .stream().map(CertificateWrapper::getSubjectSerialNumber)
-                    .filter(this::serialNumberMatch).collect(Collectors.toList());
-            if (!subjectSNCFs.isEmpty()) {
-                return subjectSNCFs;
-            }
-        }
-        return Collections.emptyList();
-    }
-
-    private boolean serialNumberMatch(String s) {
-        if (!StringUtils.isEmpty(s)) {
-            return signatureRegex.matcher(s).matches();
-        }
-        return false;
-    }
+    return false;
+  }
 
 }
--- apps/onboarding-ms/src/test/java/it/pagopa/selfcare/onboarding/controller/TokenControllerTest.java
+++ apps/onboarding-ms/src/test/java/it/pagopa/selfcare/onboarding/controller/TokenControllerTest.java
@@ -13,13 +13,16 @@ import io.quarkus.test.mongodb.MongoTestResource;
 import io.quarkus.test.security.TestSecurity;
 import io.restassured.http.ContentType;
 import io.smallrye.mutiny.Uni;
+import it.pagopa.selfcare.onboarding.controller.response.ContractSignedReport;
 import it.pagopa.selfcare.onboarding.entity.Token;
 import it.pagopa.selfcare.onboarding.exception.InvalidRequestException;
 import it.pagopa.selfcare.onboarding.service.TokenService;
 import jakarta.ws.rs.core.MediaType;
+
 import java.io.File;
 import java.util.List;
 import java.util.UUID;
+
 import org.jboss.resteasy.reactive.RestResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -29,124 +32,145 @@ import org.mockito.Mockito;
 @QuarkusTestResource(MongoTestResource.class)
 class TokenControllerTest {
 
-    @InjectMock
-    private TokenService tokenService;
+  @InjectMock
+  private TokenService tokenService;
 
-    @Test
-    @TestSecurity(user = "userJwt")
-    void getToken() {
+  @Test
+  @TestSecurity(user = "userJwt")
+  void getToken() {
 
-        final String onboardingId = "onboardingId";
-        Token token = new Token();
-        token.setId(UUID.randomUUID().toString());
-        when(tokenService.getToken(onboardingId))
-            .thenReturn(Uni.createFrom().item(List.of(token)));
+    final String onboardingId = "onboardingId";
+    Token token = new Token();
+    token.setId(UUID.randomUUID().toString());
+    when(tokenService.getToken(onboardingId))
+      .thenReturn(Uni.createFrom().item(List.of(token)));
 
-        given()
-            .when()
-            .contentType(ContentType.JSON)
-            .queryParams("onboardingId", onboardingId)
-            .get()
-            .then()
-            .statusCode(200);
-    }
+    given()
+      .when()
+      .contentType(ContentType.JSON)
+      .queryParams("onboardingId", onboardingId)
+      .get()
+      .then()
+      .statusCode(200);
+  }
 
-    @Test
-    @TestSecurity(user = "userJwt")
-    void getContract() {
-        final String onboardingId = "onboardingId";
-        RestResponse.ResponseBuilder<File> response = RestResponse.ResponseBuilder.ok();
-        when(tokenService.retrieveContractNotSigned(onboardingId))
-            .thenReturn(Uni.createFrom().item(response.build()));
+  @Test
+  @TestSecurity(user = "userJwt")
+  void getContract() {
+    final String onboardingId = "onboardingId";
+    RestResponse.ResponseBuilder<File> response = RestResponse.ResponseBuilder.ok();
+    when(tokenService.retrieveContractNotSigned(onboardingId))
+      .thenReturn(Uni.createFrom().item(response.build()));
 
-        given()
-            .when()
-            .contentType(MediaType.APPLICATION_OCTET_STREAM)
-            .get("/{onboardingId}/contract", onboardingId)
-            .then()
-            .statusCode(200);
-    }
+    given()
+      .when()
+      .contentType(MediaType.APPLICATION_OCTET_STREAM)
+      .get("/{onboardingId}/contract", onboardingId)
+      .then()
+      .statusCode(200);
+  }
 
-    @Test
-    @TestSecurity(user = "userJwt")
-    void getAttachment() {
-        final String onboardingId = "onboardingId";
-        final String filename = "filename";
-        RestResponse.ResponseBuilder<File> response = RestResponse.ResponseBuilder.ok();
-        when(tokenService.retrieveAttachment(onboardingId, filename))
-            .thenReturn(Uni.createFrom().item(response.build()));
+  @Test
+  @TestSecurity(user = "userJwt")
+  void getAttachment() {
+    final String onboardingId = "onboardingId";
+    final String filename = "filename";
+    RestResponse.ResponseBuilder<File> response = RestResponse.ResponseBuilder.ok();
+    when(tokenService.retrieveAttachment(onboardingId, filename))
+      .thenReturn(Uni.createFrom().item(response.build()));
 
-        given()
-            .when()
-            .queryParam("name", filename)
-            .contentType(MediaType.APPLICATION_OCTET_STREAM)
-            .get("/{onboardingId}/attachment", onboardingId)
-            .then()
-            .statusCode(200);
-    }
+    given()
+      .when()
+      .queryParam("name", filename)
+      .contentType(MediaType.APPLICATION_OCTET_STREAM)
+      .get("/{onboardingId}/attachment", onboardingId)
+      .then()
+      .statusCode(200);
+  }
 
-    @Test
-    @TestSecurity(user = "userJwt")
-    void getAttachmentBadRequest() {
-        final String onboardingId = "onboardingId";
-        given()
-            .when()
-            .contentType(MediaType.APPLICATION_OCTET_STREAM)
-            .get("/{onboardingId}/attachment", onboardingId)
-            .then()
-            .statusCode(400);
-    }
+  @Test
+  @TestSecurity(user = "userJwt")
+  void getAttachmentBadRequest() {
+    final String onboardingId = "onboardingId";
+    given()
+      .when()
+      .contentType(MediaType.APPLICATION_OCTET_STREAM)
+      .get("/{onboardingId}/attachment", onboardingId)
+      .then()
+      .statusCode(400);
+  }
 
-    @Test
-    @TestSecurity(user = "userJwt")
-    void updateContractSignedTest_OK() {
-        // given
-        final String onboardingId = "onboardingId";
-        final String contractSigned = "contractSigned";
+  @Test
+  @TestSecurity(user = "userJwt")
+  void updateContractSignedTest_OK() {
+    // given
+    final String onboardingId = "onboardingId";
+    final String contractSigned = "contractSigned";
 
-        Uni<Long> response = Uni.createFrom().item(Long.valueOf(1));
-        when(tokenService.updateContractSigned(onboardingId, contractSigned)).thenReturn(response);
+    Uni<Long> response = Uni.createFrom().item(Long.valueOf(1));
+    when(tokenService.updateContractSigned(onboardingId, contractSigned)).thenReturn(response);
 
-        // when
-        given()
-            .when()
-            .queryParam("onboardingId", onboardingId)
-            .queryParam("contractSigned", contractSigned)
-            .contentType(MediaType.APPLICATION_JSON)
-            .put("/contract-signed")
-            .then()
-            .statusCode(200);
+    // when
+    given()
+      .when()
+      .queryParam("onboardingId", onboardingId)
+      .queryParam("contractSigned", contractSigned)
+      .contentType(MediaType.APPLICATION_JSON)
+      .put("/contract-signed")
+      .then()
+      .statusCode(200);
 
-        // then
-        Mockito.verify(tokenService, times(1)).updateContractSigned(anyString(), anyString());
-    }
+    // then
+    Mockito.verify(tokenService, times(1)).updateContractSigned(anyString(), anyString());
+  }
 
-    @Test
-    @TestSecurity(user = "userJwt")
-    void updateContractSignedTest_KO() {
-        // given
-        final String onboardingId = "onboardingId";
-        final String contractSigned = "contractSigned";
+  @Test
+  @TestSecurity(user = "userJwt")
+  void updateContractSignedTest_KO() {
+    // given
+    final String onboardingId = "onboardingId";
+    final String contractSigned = "contractSigned";
 
-        Uni<Long> response = Uni.createFrom()
-            .failure(
-                new InvalidRequestException(
-                    String.format("Error %S", onboardingId)));
+    Uni<Long> response = Uni.createFrom()
+      .failure(
+        new InvalidRequestException(
+          String.format("Error %S", onboardingId)));
 
-        when(tokenService.updateContractSigned(onboardingId, contractSigned)).thenReturn(response);
+    when(tokenService.updateContractSigned(onboardingId, contractSigned)).thenReturn(response);
 
-        // when
-        given()
-            .when()
-            .queryParam("onboardingId", onboardingId)
-            .queryParam("contractSigned", contractSigned)
-            .contentType(MediaType.APPLICATION_JSON)
-            .put("/contract-signed")
-            .then()
-            .statusCode(400);
+    // when
+    given()
+      .when()
+      .queryParam("onboardingId", onboardingId)
+      .queryParam("contractSigned", contractSigned)
+      .contentType(MediaType.APPLICATION_JSON)
+      .put("/contract-signed")
+      .then()
+      .statusCode(400);
 
-        // then
-        Mockito.verify(tokenService, times(1)).updateContractSigned(anyString(), anyString());
-    }
+    // then
+    Mockito.verify(tokenService, times(1)).updateContractSigned(anyString(), anyString());
+  }
 
+  @Test
+  @TestSecurity(user = "userJwt")
+  void reportContractSignedTest_OK() {
+    // given
+    final String onboardingId = "onboardingId";
+
+    Uni<ContractSignedReport> response = Uni.createFrom().item(ContractSignedReport.cades(true));
+    when(tokenService.reportContractSigned(onboardingId)).thenReturn(response);
+
+    // when
+    given()
+      .when()
+      .queryParam("onboardingId", onboardingId)
+      .contentType(MediaType.APPLICATION_JSON)
+      .get("/contract-report")
+      .then()
+      .statusCode(200);
+
+    // then
+    Mockito.verify(tokenService, times(1)).reportContractSigned(anyString());
+  }
 }
