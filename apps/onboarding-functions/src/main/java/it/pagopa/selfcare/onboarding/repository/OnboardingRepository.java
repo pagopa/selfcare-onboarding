26e9539302f66e78dfedf59ebbfaaecb12444cf8
pierpaolodidato89 <137791912+pierpaolodidato89@users.noreply.github.com>
2025-01-27 09:38:19 +0100

    [SELC-6246] fix: added check for previous manager in case of new user onboarding

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/repository/OnboardingRepository.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/repository/OnboardingRepository.java
@@ -1,10 +1,22 @@
 package it.pagopa.selfcare.onboarding.repository;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepositoryBase;
+import it.pagopa.selfcare.onboarding.common.OnboardingStatus;
 import it.pagopa.selfcare.onboarding.entity.Onboarding;
+import it.pagopa.selfcare.onboarding.utils.QueryUtils;
 import jakarta.enterprise.context.ApplicationScoped;
+import java.util.List;
+import java.util.Map;
+import org.bson.Document;
 
 @ApplicationScoped
 public class OnboardingRepository implements PanacheMongoRepositoryBase<Onboarding, String> {
 
+    public List<Onboarding> findByFilters(String taxCode, String subunitCode, String origin, String originId, String productId) {
+        final Map<String, String> queryParameter =
+                QueryUtils.createMapForInstitutionOnboardingsQueryParameter(
+                        taxCode, subunitCode, origin, originId, OnboardingStatus.COMPLETED, productId);
+        Document query = QueryUtils.buildQuery(queryParameter);
+        return find(query).list();
+    }
 }
