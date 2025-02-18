e3075cfdae81bf951ae8c7472ff921da78c01212
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2025-02-18 14:34:45 +0100

    Added parameter userId for get Onboardings API

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/model/OnboardingGetFilters.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/model/OnboardingGetFilters.java
@@ -4,11 +4,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Builder
 @Getter
 @Setter
 public class OnboardingGetFilters {
     private String productId;
+    private List<String> productIds;
     private String institutionId;
     private String onboardingId;
     private String subunitCode;
@@ -16,6 +19,8 @@ public class OnboardingGetFilters {
     private String status;
     private String from;
     private String to;
+    private String userId;
     private Integer page;
     private Integer size;
+    private boolean skipPagination;
 }
