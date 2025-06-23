751573925a72126d8e6a613a97b307cf364f3bd3
andrea putzu <andrea.putzu@pagopa.it>
2025-06-23 16:53:31 +0200

    chore: Delete contract file

--- infra/resources/_modules/storage_accounts/locals.tf
+++ infra/resources/_modules/storage_accounts/locals.tf
@@ -7,4 +7,9 @@ locals {
     app_name        = var.app_name
     instance_number = var.instance_number
   }
+
+  project                  = "selc-${var.env_short}"
+  ca_resource_group_name   = "${local.project}-container-app${var.suffix_increment}-rg"
+  naming_config            = "documents"
+  resource_group_name_vnet = "${local.project}-vnet-rg"
 }
--- infra/resources/_modules/storage_accounts/variables.tf
+++ infra/resources/_modules/storage_accounts/variables.tf
@@ -1,10 +1,3 @@
-locals {
-  project                  = "selc-${var.env_short}"
-  ca_resource_group_name   = "${local.project}-container-app${var.suffix_increment}-rg"
-  naming_config            = "documents"
-  resource_group_name_vnet = "${local.project}-vnet-rg"
-}
-
 variable "app_name" {
   type        = string
   description = "App name"
