f1a762c0a608b41210693b66aa31fdb249c55198
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2025-07-22 16:00:17 +0200

    Updates

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/workflow/WorkflowExecutorContractRegistration.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/workflow/WorkflowExecutorContractRegistration.java
@@ -21,8 +21,8 @@ public record WorkflowExecutorContractRegistration(ObjectMapper objectMapper, Ta
     public Optional<OnboardingStatus> executeRequestState(TaskOrchestrationContext ctx, OnboardingWorkflow onboardingWorkflow) {
         String onboardingWorkflowString = getOnboardingWorkflowString(objectMapper, onboardingWorkflow);
         //ctx.callActivity(BUILD_CONTRACT_ACTIVITY_NAME, onboardingWorkflowString, optionsRetry, String.class).await();
-        ctx.callActivity(SAVE_TOKEN_WITH_CONTRACT_ACTIVITY_NAME, onboardingWorkflowString, optionsRetry, String.class).await();
-        ctx.callActivity(SEND_MAIL_REGISTRATION_FOR_CONTRACT, onboardingWorkflowString, optionsRetry, String.class).await();
+        //ctx.callActivity(SAVE_TOKEN_WITH_CONTRACT_ACTIVITY_NAME, onboardingWorkflowString, optionsRetry, String.class).await();
+        //ctx.callActivity(SEND_MAIL_REGISTRATION_FOR_CONTRACT, onboardingWorkflowString, optionsRetry, String.class).await();
         sendMailForUserActivity(ctx, onboardingWorkflow, onboardingMapper);
         saveVisuraActivity(ctx, onboardingWorkflow.getOnboarding());
         return Optional.of(OnboardingStatus.PENDING);
