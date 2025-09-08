b8fdff6fc73ee51acb72221cfbfd63d634a59c0e
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2025-09-08 09:35:42 +0200

    Updates

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/entity/Payment.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/entity/Payment.java
@@ -1,8 +1,9 @@
 package it.pagopa.selfcare.onboarding.entity;
 
-import it.pagopa.selfcare.onboarding.crypto.utils.DataEncryptionUtils;
 import jakarta.validation.constraints.Pattern;
+import lombok.Data;
 
+@Data
 public class Payment {
 
     @Pattern(regexp = "^IT\\d{2}[A-Z]\\d{5}\\d{5}[0-9A-Z]{12}$",
@@ -11,19 +12,4 @@ public class Payment {
 
     private String holder;
 
-    public String getHolder() {
-        return holder != null ? DataEncryptionUtils.decrypt(holder) : null;
-    }
-
-    public void setHolder(String holder) {
-        this.holder = holder != null ? DataEncryptionUtils.encrypt(holder) : null;
-    }
-
-    public String getIban() {
-        return iban != null ? DataEncryptionUtils.decrypt(iban) : null;
-    }
-
-    public void setIban(String iban) {
-        this.iban = iban != null ? DataEncryptionUtils.encrypt(iban) : null;
-    }
 }
