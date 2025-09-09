05ebc3d9b0107183f0356315f496e0ec5b43e663
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2025-09-09 11:14:36 +0200

    Added custom setters for payment obj

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/entity/Payment.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/entity/Payment.java
@@ -1,9 +1,38 @@
 package it.pagopa.selfcare.onboarding.entity;
 
+import it.pagopa.selfcare.onboarding.crypto.utils.DataEncryptionUtils;
+
+import java.util.Optional;
+
 import lombok.Data;
 
 @Data
 public class Payment {
     private String holder;
     private String iban;
+
+    public void setHolder(String holder) {
+        this.holder = Optional.ofNullable(holder)
+                .map(DataEncryptionUtils::encrypt)
+                .orElse("");
+    }
+
+    public void setIban(String iban) {
+        this.iban = Optional.ofNullable(iban)
+                .map(DataEncryptionUtils::encrypt)
+                .orElse("");
+    }
+
+    public String retrieveEncryptedHolder() {
+        return Optional.ofNullable(holder)
+                .map(DataEncryptionUtils::decrypt)
+                .orElse("");
+    }
+
+    public String retrieveEncryptedIban() {
+        return Optional.ofNullable(iban)
+                .map(DataEncryptionUtils::decrypt)
+                .orElse("");
+    }
+
 }
