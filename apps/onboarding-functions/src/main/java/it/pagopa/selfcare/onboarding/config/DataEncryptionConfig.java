ca3c23193c4d2779e42257b93e4decd8d9fd1b76
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2025-09-09 11:19:31 +0200

    Added info log for startup and read of key

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/config/DataEncryptionConfig.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/config/DataEncryptionConfig.java
@@ -4,10 +4,13 @@ import io.quarkus.runtime.Startup;
 import it.pagopa.selfcare.onboarding.crypto.utils.DataEncryptionUtils;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Singleton
 @Startup
+@Slf4j
 public class DataEncryptionConfig {
 
     @ConfigProperty(name = "onboarding-fn.data.encryption.key")
@@ -15,6 +18,7 @@ public class DataEncryptionConfig {
 
     @PostConstruct
     void init() {
+        log.info("Adding key from kv size: {} is-empty-string: {}", key.length(), StringUtils.isNotBlank(key));
         DataEncryptionUtils.setDefaultKey(key);
     }
 }
