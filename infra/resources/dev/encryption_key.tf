c092e8e65921602984b8faa9bc7153a9d3746755
pierpaolo.didato@emeal.nttdata.com <pierpaolo.didato@emeal.nttdata.com>
2025-09-22 11:05:36 +0200

    Added encription default iv

--- infra/resources/dev/encryption_key.tf
+++ infra/resources/dev/encryption_key.tf
@@ -7,6 +7,24 @@ resource "random_password" "encryption_key" {
   }
 }
 
+resource "random_password" "encryption_iv" {
+  length           = 12
+  special          = false
+  override_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+  keepers = {
+    version = 1
+  }
+}
+
+resource "azurerm_key_vault_secret" "encryption_iv_secret" {
+  name         = "onboarding-data-encryption-iv"
+  value        = random_password.encryption_iv.result
+  content_type = "text/plain"
+
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
 resource "azurerm_key_vault_secret" "encryption_key_secret" {
   name         = "onboarding-data-encryption-key"
   value        = random_password.encryption_key.result
