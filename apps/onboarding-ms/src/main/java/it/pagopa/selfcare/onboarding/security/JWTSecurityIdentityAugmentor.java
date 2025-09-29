e8e6e2eda8d0716ec4bb2e26c8da9a60d58599ab
andrea putzu <andrea.putzu@pagopa.it>
2025-09-29 17:39:31 +0200

    [SELC-7732] fix: Send SAP notification

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/security/JWTSecurityIdentityAugmentor.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/security/JWTSecurityIdentityAugmentor.java
@@ -8,59 +8,39 @@ import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
-import java.util.Set;
-
 @ApplicationScoped
 public class JWTSecurityIdentityAugmentor implements SecurityIdentityAugmentor {
 
   @Override
   public Uni<SecurityIdentity> augment(SecurityIdentity identity, AuthenticationRequestContext context) {
-//    Optional<Credential> tokenCredential = identity.getCredentials().stream()
-//      .filter(credential -> credential instanceof TokenCredential).findFirst();
-//    Optional.of(tokenCredential).ifPresent(token -> {
-//      if (token.isPresent()) {
-//        try {
-//          jwtValidator.p(((TokenCredential) token.get()).getToken());
-//        } catch (Exception e) {
-//          throw new RuntimeException(e);
-//        }
-//      }
-//    });
-//    if (identity.isAnonymous()) {
-//      return Uni.createFrom().item(identity);
+    return Uni.createFrom().item(identity)
+      .onItem().transformToUni(securityIdentity ->
+        securityIdentity.getPrincipal() instanceof JsonWebToken
+          ? Uni.createFrom().item((JsonWebToken) securityIdentity.getPrincipal())
+          : Uni.createFrom().nullItem()
+      )
+      .onItem().ifNotNull().transform(JsonWebToken::getIssuer)
+      .onItem().ifNotNull().transform(issuer -> QuarkusSecurityIdentity.builder(identity)
+        .addRole("SUPPORT")
+        .addAttribute("jwt.issuer", issuer)
+        .build())
+      .onItem().ifNull().fail().replaceWith(identity);
+  }
+
+//  private Set<String> determineRolesForIssuer(JsonWebToken jwt, String issuer) {
+//    Set<String> roles = Set.of();
+//
+//    switch (issuer) {
+//      case "SPID":
+//        roles = Set.copyOf(jwt.getClaimNames());
+//        break;
+//      case "PAGOPA":
+//        roles = Set.of("SUPPORT");
+//        break;
+//      default:
+//        roles = jwt.getGroups();
 //    }
-//    String token = extractTokenFromContext(context);
-//    JsonWebToken validatedJwt = validator.validateAndParse(identity.getPrincipal().get);
-
-    //this.jwtValidator.validateToken(identity.getCredential(JsonWebTokenCredential.class).getToken())
-    if (identity.getPrincipal() instanceof JsonWebToken) {
-      return Uni.createFrom().item((JsonWebToken) identity.getPrincipal())
-        .onItem().transform(JsonWebToken::getIssuer)
-        .onItem().transform(issuer -> {
-            QuarkusSecurityIdentity.Builder builder = QuarkusSecurityIdentity.builder(identity);
-            builder.addRole("SUPPORT");
-            builder.addAttribute("jwt.issuer", issuer);
-            return builder.build();
-        });
-    }
-
-    return Uni.createFrom().item(identity);
-  }
-
-  private Set<String> determineRolesForIssuer(JsonWebToken jwt, String issuer) {
-    Set<String> roles = Set.of();
-
-    switch (issuer) {
-      case "SPID":
-        roles = Set.copyOf(jwt.getClaimNames());
-        break;
-      case "PAGOPA":
-        roles = Set.of("SUPPORT");
-        break;
-      default:
-        roles = jwt.getGroups();
-    }
-
-    return roles != null ? roles : Set.of();
-  }
+//
+//    return roles != null ? roles : Set.of();
+//  }
 }
