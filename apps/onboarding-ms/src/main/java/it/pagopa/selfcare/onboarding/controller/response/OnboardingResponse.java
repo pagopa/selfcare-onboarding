d4fa2f229f269387e798c355b1fffefeef67ff08
pierpaolo.didato@emeal.nttdata.com <pierpaolo.didato@emeal.nttdata.com>
2025-11-10 09:45:12 +0100

    Updates

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/controller/response/OnboardingResponse.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/controller/response/OnboardingResponse.java
@@ -21,5 +21,6 @@ public class OnboardingResponse {
     private Boolean isAggregator;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+    private String referenceOnboardingId;
 
 }
