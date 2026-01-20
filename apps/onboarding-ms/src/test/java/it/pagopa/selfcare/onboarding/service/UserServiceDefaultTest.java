d6c009f5bb9ed7a13abe71f027e5bfb2598044c6
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-01-20 16:51:48 +0100

    Added junit test case for new service

---
+++ apps/onboarding-ms/src/test/java/it/pagopa/selfcare/onboarding/service/UserServiceDefaultTest.java
@@ -0,0 +1,54 @@
+package it.pagopa.selfcare.onboarding.service;
+
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.mutiny.Uni;
+import it.pagopa.selfcare.onboarding.service.impl.UserServiceDefault;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Test;
+import org.openapi.quarkus.user_json.api.InstitutionApi;
+import org.openapi.quarkus.user_json.model.UserInstitutionResponse;
+
+import java.util.List;
+
+import static org.junit.Assert.assertSame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@QuarkusTest
+class UserServiceDefaultTest {
+
+    @Inject
+    UserServiceDefault userServiceDefault;
+
+    @InjectMock
+    @RestClient
+    InstitutionApi userInstitutionApi;
+
+    @Test
+    void retrieveUserInstitutionsTest() {
+        // Given
+        String institutionId = "institutionId";
+        List<String> productRoles = List.of();
+        List<String> products = List.of();
+        List<String> roles = List.of();
+        List<String> states = List.of();
+        String userId = "userId";
+
+        Uni<List<UserInstitutionResponse>> expected = Uni.createFrom().item(List.of());
+
+        when(userInstitutionApi.retrieveUserInstitutions(any(), any(), any(), any(), any(), any()))
+                .thenReturn(expected);
+
+        // When
+        Uni<List<UserInstitutionResponse>> result =
+                userServiceDefault.retrieveUserInstitutions(institutionId, productRoles, products, roles, states, userId);
+
+        // Then
+        assertSame(expected, result);
+        verify(userInstitutionApi)
+                .retrieveUserInstitutions(institutionId, productRoles, products, roles, states, userId);
+        verifyNoMoreInteractions(userInstitutionApi);
+    }
+}
