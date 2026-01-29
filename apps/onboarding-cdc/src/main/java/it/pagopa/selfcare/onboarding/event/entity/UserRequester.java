4ac72daea54224439cec4b4017f90097922f6d3e
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-01-29 15:31:53 +0100

    Fixed lombok

--- apps/onboarding-cdc/src/main/java/it/pagopa/selfcare/onboarding/event/entity/UserRequester.java
+++ apps/onboarding-cdc/src/main/java/it/pagopa/selfcare/onboarding/event/entity/UserRequester.java
@@ -1,14 +1,8 @@
 package it.pagopa.selfcare.onboarding.event.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserRequester {
     private String userRequestUid;
     private String userMailUuid;
