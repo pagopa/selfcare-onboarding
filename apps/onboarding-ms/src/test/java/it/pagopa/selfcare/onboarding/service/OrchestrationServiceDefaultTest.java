9d5f55d06b1023520108b77d19646109d2247ade
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-02-09 11:14:00 +0100

    Fixed junit test

--- apps/onboarding-ms/src/test/java/it/pagopa/selfcare/onboarding/service/OrchestrationServiceDefaultTest.java
+++ apps/onboarding-ms/src/test/java/it/pagopa/selfcare/onboarding/service/OrchestrationServiceDefaultTest.java
@@ -7,34 +7,42 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.helpers.test.UniAssertSubscriber;
 import it.pagopa.selfcare.onboarding.service.impl.OrchestrationServiceDefault;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.Test;
+import org.openapi.quarkus.onboarding_functions_json.api.OrchestrationApi;
 import org.openapi.quarkus.onboarding_functions_json.model.OrchestrationResponse;
 
 @QuarkusTest
 class OrchestrationServiceDefaultTest {
 
+    @Inject
+    OrchestrationServiceDefault orchestrationServiceDefault;
+
     @InjectMock
-    OrchestrationServiceDefault service;
+    @RestClient
+    OrchestrationApi orchestrationApi;
+
 
     @Test
     void triggerOrchestration_success() {
         // given
         String onboardingId = "onb-123";
         OrchestrationResponse response = mock(OrchestrationResponse.class);
-        when(service.triggerOrchestration(
+        when(orchestrationApi.apiStartOnboardingOrchestrationGet(
                 onboardingId, null))
                 .thenReturn(Uni.createFrom().item(response));
 
         // when
-        Uni<OrchestrationResponse> uni = service.triggerOrchestration(onboardingId, null);
+        Uni<OrchestrationResponse> uni = orchestrationServiceDefault.triggerOrchestration(onboardingId, null);
 
         // then
         UniAssertSubscriber<OrchestrationResponse> sub = uni.subscribe()
                 .withSubscriber(UniAssertSubscriber.create());
         sub.assertCompleted().assertItem(response);
-        verify(service).triggerOrchestration(
+        verify(orchestrationApi).apiStartOnboardingOrchestrationGet(
                 onboardingId, null);
-        verifyNoMoreInteractions(service);
+        verifyNoMoreInteractions(orchestrationApi);
     }
 
     @Test
@@ -42,58 +50,55 @@ class OrchestrationServiceDefaultTest {
         // given
         String onboardingId = "onb-err";
         RuntimeException boom = new RuntimeException("boom");
-        when(service.triggerOrchestration(
+        when(orchestrationApi.apiStartOnboardingOrchestrationGet(
                 onboardingId, null))
                 .thenReturn(Uni.createFrom().failure(boom));
 
         // when
-        Uni<OrchestrationResponse> uni = service.triggerOrchestration(onboardingId, null);
+        Uni<OrchestrationResponse> uni = orchestrationServiceDefault.triggerOrchestration(onboardingId, null);
 
         // then
         UniAssertSubscriber<OrchestrationResponse> sub = uni.subscribe()
                 .withSubscriber(UniAssertSubscriber.create());
         sub.assertFailedWith(RuntimeException.class);
-        verify(service).triggerOrchestration(
+        verify(orchestrationApi).apiStartOnboardingOrchestrationGet(
                 onboardingId, null);
-        verifyNoMoreInteractions(service);
-    }
+        verifyNoMoreInteractions(orchestrationApi);    }
 
     @Test
     void triggerOrchestrationDeleteInstitutionAndUser_success() {
         // given
         String onboardingId = "onb-del-123";
         OrchestrationResponse response = mock(OrchestrationResponse.class);
-        when(service.triggerOrchestrationDeleteInstitutionAndUser(onboardingId))
+        when(orchestrationApi.apiTriggerDeleteInstitutionAndUserGet(onboardingId))
                 .thenReturn(Uni.createFrom().item(response));
 
         // when
-        Uni<OrchestrationResponse> uni = service.triggerOrchestrationDeleteInstitutionAndUser(onboardingId);
+        Uni<OrchestrationResponse> uni = orchestrationServiceDefault.triggerOrchestrationDeleteInstitutionAndUser(onboardingId);
 
         // then
         UniAssertSubscriber<OrchestrationResponse> sub = uni.subscribe()
                 .withSubscriber(UniAssertSubscriber.create());
         sub.assertCompleted().assertItem(response);
-        verify(service).triggerOrchestrationDeleteInstitutionAndUser(onboardingId);
-        verifyNoMoreInteractions(service);
-    }
+        verify(orchestrationApi).apiTriggerDeleteInstitutionAndUserGet(onboardingId);
+        verifyNoMoreInteractions(orchestrationApi);    }
 
     @Test
     void triggerOrchestrationDeleteInstitutionAndUser_failure() {
         // given
         String onboardingId = "onb-del-err";
         IllegalStateException failure = new IllegalStateException("cannot delete");
-        when(service.triggerOrchestrationDeleteInstitutionAndUser(onboardingId))
+        when(orchestrationApi.apiTriggerDeleteInstitutionAndUserGet(onboardingId))
                 .thenReturn(Uni.createFrom().failure(failure));
 
         // when
-        Uni<OrchestrationResponse> uni = service.triggerOrchestrationDeleteInstitutionAndUser(onboardingId);
+        Uni<OrchestrationResponse> uni = orchestrationServiceDefault.triggerOrchestrationDeleteInstitutionAndUser(onboardingId);
 
         // then
         UniAssertSubscriber<OrchestrationResponse> sub = uni.subscribe()
                 .withSubscriber(UniAssertSubscriber.create());
         sub.assertFailedWith(IllegalStateException.class);
-        verify(service).triggerOrchestrationDeleteInstitutionAndUser(onboardingId);
-        verifyNoMoreInteractions(service);
+        verify(orchestrationApi).apiTriggerDeleteInstitutionAndUserGet(onboardingId);
+        verifyNoMoreInteractions(orchestrationApi);
     }
 }
-
